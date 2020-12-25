9afae72a3aa1886bbaf5bae51c9b3bb96fb69526
thomasvergne <thomas@quark-lang.dev>
2020-12-25 20:19:19 +0100

    ✨ Process method added.

--- src/core/parser.ts
+++ src/core/parser.ts
@@ -4,6 +4,13 @@ import { Block } from '../typings/block.ts';
 type Node = Record<string , string | Node[]>;
 
 export class Parser {
+  private static ast: Node = {
+    type: 'Program',
+    body: [],
+  }
+  private static process(index: number, ast: Block, newAst: Node) {
+    return this.ast;
+  }
 
   public static parse(source: string) {
     const ast: Block = Processor.process(source);
