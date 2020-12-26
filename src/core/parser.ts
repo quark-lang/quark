f55df7e7af6c0f89f95007f58240e7743b2d793d
thomasvergne <thomas@quark-lang.dev>
2020-12-26 12:11:57 +0100

    🏷️ AST Node type added.

--- src/core/parser.ts
+++ src/core/parser.ts
@@ -1,9 +1,12 @@
 import { Processor } from './processor.ts';
 import { Block } from '../typings/block.ts';
 
+type Node = Record<string , string | Node[]>;
+
 export class Parser {
 
   public static parse(source: string) {
-
+    const ast: Block = Processor.process(source);
+    return this.process(0, ast, this.ast);
   }
 }
