68c54d3cc3be1b2eb172b3f500646289c513071d
thomasvergne <thomas@quark-lang.dev>
2020-12-28 21:28:41 +0100

    ➕ Main imports updated.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { File } from './utils/file.ts';
-import { Processor } from './core/processor.ts';
+import { Parser } from './core/parser.ts';
 import { getCircularReplacer } from './utils/json.ts';
 
 async function main(): Promise<void> {
   // Getting sample code content and interpreting it
   const script: string = await File.read('sample/index.qrk');
-  const ast = Processor.process(script);
+  const ast = Parser.parse(script);
 
   console.log(JSON.stringify(ast, getCircularReplacer(), 2))
 }
