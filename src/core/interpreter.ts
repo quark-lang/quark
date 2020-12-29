0273c9948fde57888e9199279ae084c25a573b9b
thomasvergne <thomas@quark-lang.dev>
2020-12-29 12:28:15 +0100

    ✨ Basic interpreter logic added.

--- src/core/interpreter.ts
+++ src/core/interpreter.ts
@@ -3,9 +3,28 @@ import { Parser } from './parser.ts';
 
 export class Interpreter {
   private static ast: Block;
+  private static process(node: Block, state?: string) {
+    for (const child of node) {
+      if (Array.isArray(child)) {
+        this.process(child);
+      } else {
+        const [expression, ...args] = node;
+        if (expression === 'let') {
+          console.log('Identifier:', this.process(args[0] as Block, 'Identifier'));
+          console.log('Value:', this.process(args[1] as Block));
+        } else if (expression === '+') {
+          if (state && state === 'Identifier') {
+            args[0] = args[0].slice(1, args[0].length - 1);
+            args[1] = args[1].slice(1, args[1].length - 1);
+          }
+          return args[0] + <string>args[1];
+        }
+      }
+    }
+  }
   public static run(source: string) {
     this.ast = Parser.parse(source);
-    console.log(this.ast);
+    this.process(this.ast);
     return this.ast;
   }
 }
