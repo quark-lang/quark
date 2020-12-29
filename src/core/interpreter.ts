ff66f34fbd40fbf01c1121285e4851a0596691b9
thomasvergne <thomas@quark-lang.dev>
2020-12-29 14:00:59 +0100

    ➕ Stack variable quark implemented.

--- src/core/interpreter.ts
+++ src/core/interpreter.ts
@@ -4,12 +4,16 @@ import { Parser } from './parser.ts';
 export class Interpreter {
   private static stack: Record<any, any> = {};
   private static ast: Block;
-  private static process(node: Block | Element, state?: string): Block | Element | string {
+  private static process(node: Block | Element, state?: string): Block | Element | string | Record<any, any> {
     if ('value' in node) {
       if (state && state === 'Identifier') return node.value as string;
-      if (node.type === 'Word') return this.stack[node.value] ? this.stack[node.value] : node.value;
+      if (node.type === 'Word') {
+        if (node.value === 'stack') return this.stack;
+        if (this.stack[node.value]) return this.stack[node.value];
+        return node.value as string;
+      }
       return node.value as string;
-    };
+    }
     for (const child of node) {
       if (Array.isArray(child)) {
         this.process(child);
@@ -21,11 +25,18 @@ export class Interpreter {
             if (state === 'Identifier') {
               (<Element[]>args).map((arg) => arg.type = 'Word');
             };
-            const parsedArgs = args.map((acc) => this.process(acc) as string);
+            const parsedArgs = args.map((acc) => {
+              if ('value' in acc && acc.type === 'Number') {
+                return Number(this.process(acc)) as any;
+              }
+              return this.process(acc) as any;
+            });
+            if (parsedArgs.length === 0) return '';
             return parsedArgs.reduce((acc, cur) => acc + cur);
           } else if (expr.value === 'let') {
             const id: string = this.process(args[0] as Block, 'Identifier') as string;
             this.stack[id] = this.process(args[1] as Block);
+            return this.stack[id];
           } else if (expr.value === 'print') {
             console.log(...args.map((arg) => this.process(arg)));
             return node;
