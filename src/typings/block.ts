c35443e682847420d728abbd700c7fe493b80d7e
thomasvergne <thomas@quark-lang.dev>
2020-12-29 13:41:03 +0100

    🏷️ Element interface added.

--- src/typings/block.ts
+++ src/typings/block.ts
@@ -1,1 +1,11 @@
-export type Block = (string | Block)[];
+export type Block = (Element | Block)[];
+
+export type ElementTypes =
+  | 'String'
+  | 'Number'
+  | 'Word';
+
+export interface Element {
+  type: ElementTypes,
+  value: string | number,
+}
