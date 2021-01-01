ee21e3b15ee6df52a9e0a67a0accfac1caeaf53e
thomasvergne <thomas@quark-lang.dev>
2021-01-01 22:44:58 +0100

    ✨ Function call and definition added.

--- src/core/interpreter.ts
+++ src/core/interpreter.ts
@@ -23,7 +23,7 @@ export class Interpreter {
   }
 
   private static variableDefinition(node: Block) {
-    this.stack[this.processValue(<Element>node[0], 'Identifier')] = this.processValue(<Element>node[1]);
+    this.stack[this.processValue(<Element>node[0], 'Identifier')] = this.process(<Element>node[1]);
   }
 
   private static processValue(element: Element, state?: string) {
@@ -33,6 +33,36 @@ export class Interpreter {
       return 'none';
     }
     return element.value as string;
+  }
+
+  private static processArithmetic(operation: string, args: Block) {
+    switch (operation) {
+      case '+': return args.reduce((acc: any, cur: any) => this.process(acc) + this.process(cur));
+    }
+  }
+
+  private static functionDefinition(node: Block) {
+    const [args, body] = node;
+    const functionArguments: string[] = [];
+    // @ts-ignore
+    for (const arg of args) {
+      const argumentName: string = this.processValue(<Element>arg, 'Identifier');
+      functionArguments.push(argumentName);
+    }
+    return {
+      args: functionArguments,
+      body,
+      type: 'Function'
+    };
+  }
+
+  private static callFunction(node: Block, functionName: string) {
+    const values = [];
+    for (const arg of node) values.push(this.process(arg));
+    this.addStack();
+    const fn = this.stack[functionName]
+    for (const index in fn.args) this.stack[fn.args[index]] = values[Number(index)];
+    this.process(fn.body);
   }
 
   private static process(node: Block | Element): any | void {
@@ -50,17 +80,16 @@ export class Interpreter {
             this.process(args);
             this.removeStack();
           }
-          if (expr.value === 'let') this.variableDefinition(args);
-          else if (expr.value === 'print') {
-            console.log(...args.map(this.process));
-          };
+          else if (expr.value === 'let') this.variableDefinition(args);
+          else if (expr.value === 'print') console.log(...args.map(this.process));
+          else if (expr.value === '+') return Interpreter.processArithmetic(expr.value, args);
+          else if (expr.value === 'fn') return Interpreter.functionDefinition(args);
+          else if (this.stack[expr.value].type === 'Function') this.callFunction(args, expr.value as string);
           return node;
         }
       }
     }
-    if (returned) {
-      return returned;
-    }
+    if (returned) return returned;
     return 'none';
   }
   public static run(source: string, cwd?: string) {
