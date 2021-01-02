f3bcac0d1198c1ceb2ce4d9be0c0a4a17bcca996
thomasvergne <thomas@quark-lang.dev>
2021-01-02 11:59:51 +0100

    ✨ JS Modules interpretation added.

--- src/core/interpreter.ts
+++ src/core/interpreter.ts
@@ -23,9 +23,9 @@ export class Interpreter {
     return this.stack;
   }
 
-  private static variableDefinition(node: Block) {
-    const variable = this.process(<Element>node[0], 'Identifier');
-    const value = this.process(<Element>node[1]);
+  private static async variableDefinition(node: Block) {
+    const variable = await this.process(<Element>node[0], 'Identifier');
+    const value = await this.process(<Element>node[1]);
     if (typeof variable !== 'string' && 'variable' in variable) {
       const currentStackVariable = this.stack[variable.variable];
       if (typeof currentStackVariable === 'string') {
@@ -35,7 +35,7 @@ export class Interpreter {
       }
       else currentStackVariable[variable.index] = value;
     }
-    else this.stack[this.process(<Element>node[0], 'Identifier')] = value;
+    else this.stack[await this.process(<Element>node[0], 'Identifier')] = value;
   }
 
   private static processValue(element: Element, state?: string) {
@@ -49,9 +49,10 @@ export class Interpreter {
     return element.value as string;
   }
 
-  private static processArithmetic(operation: string, args: Block) {
+  private static async processArithmetic(operation: string, args: Block): Promise<any> {
     switch (operation) {
-      case '+': return args.reduce((acc: any, cur: any) => this.process(acc) + this.process(cur));
+      case '+': // @ts-ignore
+        return args.reduce(async (acc: any, cur: any) => (await this.process(acc)) + (await this.process(cur)));
       case '-': // @ts-ignore
         return args.reduce((acc: any, cur: any) => this.process(acc) - this.process(cur));
       case '*':
@@ -63,16 +64,18 @@ export class Interpreter {
     }
   }
 
-  private static processList(args: Block): Block {
-    return [...args.map((arg: Block | Element) => this.process(arg))]
+  private static async processList(args: Block): Promise<any> {
+    const array = [];
+    for (const arg of args) array.push(await this.process(arg));
+    return array;
   }
 
-  private static functionDefinition(node: Block) {
+  private static async functionDefinition(node: Block) {
     const [args, body] = node;
     const functionArguments: string[] = [];
     // @ts-ignore
     for (const arg of args) {
-      const argumentName: string = this.processValue(<Element>arg, 'Identifier');
+      const argumentName: string = await this.processValue(<Element>arg, 'Identifier');
       functionArguments.push(argumentName);
     }
     return {
@@ -82,14 +85,15 @@ export class Interpreter {
     };
   }
 
-  private static callFunction(node: Block, functionName: string) {
+  private static async callFunction(node: Block, functionName: string) {
     const values = [];
-    for (const arg of node) values.push(this.process(arg));
+    for (const arg of node) values.push(await this.process(arg));
+    if (this.stack[functionName].js === true) return this.stack[functionName].func(...values);
     this.pushStackFrame();
     const fn = this.stack[functionName]
     for (const index in fn.args) this.stack[fn.args[index]] = values[Number(index)];
     for (const el of fn.body) {
-      const res = this.process(el);
+      const res = await this.process(el);
       if (res && res[1] && res[1] === true) {
         for (const arg of fn.args) delete this.stack[arg];
         return res[0];
@@ -98,54 +102,72 @@ export class Interpreter {
     const lastStatement = fn.body.slice(-1)[0];
     for (const arg of fn.args) delete this.stack[arg];
     if (lastStatement.length > 1 && lastStatement[0].value !== 'return') return 'none';
-    if (lastStatement) return this.process(lastStatement);
+    if (lastStatement) return await this.process(lastStatement);
     this.popStackFrame();
     return 'none';
   }
 
-  private static processReturn(node: Block) {
-    const value = this.process(node[0]);
+  private static async processReturn(node: Block) {
+    const value = await this.process(node[0]);
     this.popStackFrame();
     return [value, true];
   }
 
-  private static processCondition(node: Block) {
-    if (this.process(node[0])) return this.process(node[1]);
+  private static async processCondition(node: Block) {
+    if (await this.process(node[0])) return this.process(node[1]);
     if (node[2]) return this.process(node[2]);
     return [undefined, false]
   }
 
-  private static processEqualities(operation: string, lhs: Block | Element, rhs: Block | Element) {
+  private static async processEqualities(operation: string, lhs: Block | Element, rhs: Block | Element) {
     switch (operation) {
-      case '=': return this.process(lhs) == this.process(rhs);
-      case '!=': return this.process(lhs) != this.process(rhs);
-      case '<': return this.process(lhs) < this.process(rhs);
-      case '>': return this.process(lhs) > this.process(rhs);
-      case '<=': return this.process(lhs) <= this.process(rhs);
-      case '>=': return this.process(lhs) >= this.process(rhs);
-      case 'and': return this.process(lhs) && this.process(rhs);
-      case 'or': return this.process(lhs) || this.process(rhs);
+      case '=': return await this.process(lhs) == await this.process(rhs);
+      case '!=': return await this.process(lhs) != await this.process(rhs);
+      case '<': return await this.process(lhs) < await this.process(rhs);
+      case '>': return await this.process(lhs) > await this.process(rhs);
+      case '<=': return await this.process(lhs) <= await this.process(rhs);
+      case '>=': return await this.process(lhs) >= await this.process(rhs);
+      case 'and': return await this.process(lhs) && await this.process(rhs);
+      case 'or': return await this.process(lhs) || await this.process(rhs);
     }
   }
 
-  private static processImport(node: Block) {
+  private static async processImport(node: Block) {
     const path = this.cwd + '/' + (<Element>node[0]).value;
     if (!existsSync(path)) throw `File ${path} does not exists!`;
-    const array = Deno.readFileSync(path);
-    const content: string = new TextDecoder('utf-8').decode(array);
+    if (!path.endsWith('.js')) {
+      const array = Deno.readFileSync(path);
+      const content: string = new TextDecoder('utf-8').decode(array);
 
-    const ast = Parser.parse(content, true);
-    this.process(ast);
-  }
-
-  private static processWhile(node: Block) {
-    const [condition, body] = node;
-    while (this.process(condition)) {
-      this.process(body);
+      const ast = Parser.parse(content, true);
+      await this.process(ast);
+    } else {
+      const absolutePath = import.meta.url.split('src/core/interpreter.ts').join('') + path;
+      const { module, namespace } = (await import(absolutePath));
+      if (Array.isArray(module)) {
+        for (const func of module) {
+          this.stack[namespace ? namespace + ':' + func.name : func.name] = {
+            type: 'Function',
+            js: true,
+            func: func.func,
+          };
+        }
+      } else this.stack[namespace ? namespace + ':' + module.name : module.name] = {
+        type: 'Function',
+        js: true,
+        func: module.func,
+      };
     }
   }
 
-  private static processListIndex(node: Block, state?: string) {
+  private static async processWhile(node: Block) {
+    const [condition, body] = node;
+    while (await this.process(condition)) {
+      await this.process(body);
+    }
+  }
+
+  private static async processListIndex(node: Block, state?: string) {
     const [array, index] = node;
     if (state && state === 'Identifier') {
       return {
@@ -153,38 +175,37 @@ export class Interpreter {
         index: this.process(index),
       };
     }
-    return this.process(array)[this.process(index)];
+    return (await this.process(array))[await this.process(index)];
   }
 
-  private static process(node: Block | Element, state?: string): any {
+  private static async process(node: Block | Element, state?: string): Promise<any> {
     let returned;
     if (typeof node === 'string') return node;
     if ('value' in node) return Interpreter.processValue(node, state);
     for (const child of node) {
       if (Array.isArray(child)) {
-        returned = this.process(child);
+        returned = await this.process(child);
       } else {
         const [expression, ...args] = node;
         if ('value' in (expression as Element)) {
           const expr = <Element>expression;
           if (expr.value === '{') {
             this.pushStackFrame();
-            this.process(args);
+            await this.process(args);
             this.popStackFrame();
           }
-          else if (expr.value === 'import') Interpreter.processImport(args);
-          else if (expr.value === 'let') this.variableDefinition(args);
-          else if (expr.value === 'print') console.log(...args.map(arg => this.process(arg)));
-          else if (['+', '-', '/', '*'].includes(expr.value as string)) return Interpreter.processArithmetic(expr.value as string, args);
-          else if (expr.value === 'fn') return Interpreter.functionDefinition(args);
-          else if (expr.value === 'return') return Interpreter.processReturn(args);
-          else if (['=', '!=', '<', '>', '<=', '>=', 'and', 'or'].includes(expr.value as string)) return Interpreter.processEqualities(expr.value as string, args[0], args[1]);
-          else if (expr.value === 'if') return Interpreter.processCondition(args);
-          else if (expr.value === 'list') return Interpreter.processList(args);
-          else if (expr.value === 'while') return Interpreter.processWhile(args);
-          else if (expr.value === 'index') return Interpreter.processListIndex(args, state);
-          else if (Interpreter.stack[expr.value] && Interpreter.stack[expr.value].type === 'Function') return Interpreter.callFunction(args, expr.value as string);
-          return node;
+          else if (expr.value === 'import') return await Interpreter.processImport(args);
+          else if (expr.value === 'let') return await this.variableDefinition(args);
+          else if (['+', '-', '/', '*'].includes(expr.value as string)) return await Interpreter.processArithmetic(expr.value as string, args);
+          else if (expr.value === 'fn') return await Interpreter.functionDefinition(args);
+          else if (expr.value === 'return') return await Interpreter.processReturn(args);
+          else if (['=', '!=', '<', '>', '<=', '>=', 'and', 'or'].includes(expr.value as string)) return await Interpreter.processEqualities(expr.value as string, args[0], args[1]);
+          else if (expr.value === 'if') return await Interpreter.processCondition(args);
+          else if (expr.value === 'list') return await Interpreter.processList(args);
+          else if (expr.value === 'while') return await Interpreter.processWhile(args);
+          else if (expr.value === 'index') return await Interpreter.processListIndex(args, state);
+          else if (Interpreter.stack[expr.value] && Interpreter.stack[expr.value].type === 'Function') return await Interpreter.callFunction(args, expr.value as string);
+          throw `Function or keyword ${expr.value} does not exists!`;
         }
       }
     }
@@ -193,10 +214,10 @@ export class Interpreter {
     }
     return 'none';
   }
-  public static run(source: string, cwd?: string) {
+  public static async run(source: string, cwd?: string) {
     this.ast = Parser.parse(source);
     this.cwd = cwd || Deno.cwd();
-    this.process(this.ast);
+    await this.process(this.ast);
     return undefined;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { Interpreter } from './core/interpreter.ts';
 async function main(): Promise<void> {
   // Getting sample code content and interpreting it
   const script: string = await File.read('sample/index.qrk');
-  Interpreter.run(script, 'sample');
+  await Interpreter.run(script, 'sample');
 
 }
 
