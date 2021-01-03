853288a2fa8e667e531525b7578a352cc2d7d582
NessMC <contact@nessmc.fr>
2021-01-03 15:59:46 +0100

    ✨ CLI default script added.

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ import * as path from 'https://deno.land/std@0.83.0/path/mod.ts';
 async function main(): Promise<void> {
   // Getting sample code content and interpreting it
   const cwd = path.resolve();
-  const script: string = await File.read(path.join(cwd, 'sample', 'main.qrk'));
-  await Interpreter.run(script, cwd, 'sample');
+  const script: string = await File.read(path.join(cwd, 'cli', 'main.qrk'));
+  await Interpreter.run(script, cwd, 'cli');
 
 }
 
