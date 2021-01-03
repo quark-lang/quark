2c8030aff9374321ed1c0f1d5c4e60ecadf79a91
NessMC <contact@nessmc.fr>
2021-01-03 11:12:56 +0100

    🍱 Input function added.

--- sample/print.js
+++ sample/print.js
@@ -1,4 +1,17 @@
-export const module = {
-  name: 'print',
-  func: console.log,
-}
+export const module = [
+  {
+    name: 'print',
+    func: console.log
+  },
+  {
+    name: 'input',
+    func: async (question = '') => {
+      const buf = new Uint8Array(1024);
+      await Deno.stdout.write(new TextEncoder().encode(question));
+
+      const input = await Deno.stdin.read(buf);
+      const answer = new TextDecoder().decode(buf.subarray(0, input));
+      return answer.trim();
+    }
+  }
+]
