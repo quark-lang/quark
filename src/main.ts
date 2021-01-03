61837d1de61a0515113c0923767c6f37b1dd2628
NessMC <contact@nessmc.fr>
2021-01-03 13:20:27 +0100

    🍱 Main script added.

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as path from 'https://deno.land/std@0.83.0/path/mod.ts';
 async function main(): Promise<void> {
   // Getting sample code content and interpreting it
   const cwd = path.resolve();
-  const script: string = await File.read(path.join(cwd, 'sample', 'index.qrk'));
+  const script: string = await File.read(path.join(cwd, 'sample', 'main.qrk'));
   await Interpreter.run(script, cwd, 'sample');
 
 }
