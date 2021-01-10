66af87b02ad9db9400fed8a2577d41870ee0d49e
thomasvergne <thomas@quark-lang.dev>
2021-01-10 14:16:58 +0100

    ✨ Quark API updated with new core.

--- api/api.ts
+++ api/api.ts
@@ -1,6 +1,7 @@
-import { Interpreter } from '../src/core/interpreter.ts';
+import { Interpreter, Frame, Function, Types, Variable, ValueElement } from '../src/core/interpreter.ts';
 import { QuarkTypes } from './typings/types.ts';
 import { QuarkCallback } from './typings/callback.ts';
+import { Block } from '../src/typings/block.ts';
 
 export interface QuarkDefinition {
   name: string,
@@ -13,29 +14,32 @@ export interface QuarkFunction extends QuarkDefinition {
 
 export interface QuarkVariable extends QuarkDefinition {
   name: string,
-  value: any,
+  value: ValueElement,
 }
 
 export class QuarkModule {
-  public static stack = Interpreter.stack;
+  public static stack = Frame.stack;
   public static cwd = Interpreter.cwd;
   public static declare(namespace: string | null, type: QuarkTypes, definition: QuarkVariable | QuarkFunction): QuarkDefinition {
     const ns: string = namespace ? `${namespace}:${definition.name}` : definition.name;
     if (type === QuarkTypes.QuarkFunction) {
       definition = <QuarkFunction>definition;
-      Interpreter.stack[ns] = {
-        type: 'Function',
-        js: true,
-        func: definition.body,
-      };
+      Frame.frame.variables.push({
+        name: ns,
+        value: {
+          type: Types.Function,
+          args: [],
+          js: true,
+          body: definition.body as (() => {}),
+        },
+      });
     } else if (type === QuarkTypes.QuarkVariable) {
       definition = <QuarkVariable>definition;
-      Interpreter.stack[ns] = definition.value;
+      Frame.frame.variables.push({
+        name: ns,
+        value: definition.value,
+      })
     }
     return definition;
-  }
-
-  public static async createCall(fn: QuarkCallback, ...args: any[]) {
-    return await Interpreter.callFunction(args, fn);
   }
 }
