cdb3b91f18ebea174d71ba1edd5c741e75cc1f1e
thomasvergne <thomas@quark-lang.dev>
2021-01-12 11:46:42 +0100

    🐛 Quark CLI location fixed.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { File } from './utils/file.ts';
-import { getCircularReplacer } from './utils/json.ts';
 import { Interpreter } from './core/interpreter.ts';
 import * as path from 'https://deno.land/std@0.83.0/path/mod.ts';
 
 async function main(): Promise<void> {
   // Getting sample code content and interpreting it
-  const cwd = Deno.cwd();
-  const src = path.join(cwd, 'cli', 'main.qrk');
+  const src = path.join(path.dirname(path.fromFileUrl(import.meta.url)), '..', 'cli', 'main.qrk');
   const script: string = await File.read(src);
   await Interpreter.run(script, src);
 
