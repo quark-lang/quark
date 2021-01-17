b34e452ccaf7574cf2fcd1b4782d5b909941e44b
NessMC <contact@nessmc.fr>
2021-01-17 17:12:32 +0100

    ✨ Get value and quarkify functions added.

--- api/api.ts
+++ api/api.ts
@@ -17,6 +17,21 @@ export interface QuarkVariable extends QuarkDefinition {
   value: ValueElement,
 }
 
+export function quarkify(fn: (...data: any[]) => any, ...args: ValueElement[]): any {
+  return fn.call(fn, ...getValue(args));
+}
+
+export function getValue(values: ValueElement[]): any {
+  let result: any = [];
+  for (const value of values) {
+    if (value.type === Types.List) {
+      result.push(getValue(value.value));
+    } else if ('value' in value) result.push(value.value === undefined ? 'none' : value.value);
+    else result.push('none');
+  }
+  return result;
+}
+
 export class QuarkModule {
   public static stack = Frame.stack;
   public static cwd = Interpreter.cwd;
