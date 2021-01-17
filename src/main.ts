23b9a2167df389b88a15dfc8b4b24e969f140f2b
NessMC <contact@nessmc.fr>
2021-01-17 11:49:48 +0100

    ✨ Quark root folder getter function added.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
-import { File } from './utils/file.ts';
-import { Interpreter } from './core/interpreter.ts';
+import {File} from './utils/file.ts';
+import {Interpreter} from './core/interpreter.ts';
 import * as path from 'https://deno.land/std@0.83.0/path/mod.ts';
+import {existsSync} from 'https://deno.land/std/fs/mod.ts';
 import '../std/mod.ts'; // Importing Typescript STD by default.
 import '../std/quark.ts';
 
+export async function getQuarkFolder(): Promise<string> {
+  const dir = await Deno.realPath('.');
+  const condition = path.basename(dir) === 'quark' || (existsSync(path.join(dir, 'cli')) && existsSync(path.join(dir, 'std')));
+  return !condition
+    ? (<string>Deno.env.get('Path'))
+      .split(';')
+      .find((x) => x.includes('quark')) || ''
+    : '';
+}
+
 async function main(): Promise<void> {
   // Getting sample code content and interpreting it
-  const src = path.join('cli', 'main.qrk');
+  const root: string = await getQuarkFolder();
+  const src = path.join(<string>root, 'cli', 'main.qrk');
   const script: string = await File.read(src);
   await Interpreter.run(script, src);
-
 }
 
 await main();
