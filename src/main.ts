df3e29e5ad462c68b7eef2fce51978ce57b93a79
thomasvergne <thomas@quark-lang.dev>
2021-01-27 23:37:38 +0100

    ♻️ Main file refactored.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {File} from './utils/file.ts';
+import { File } from './utils/file.ts';
 import { Interpreter } from './core/interpreter.ts';
 import * as path from 'https://deno.land/std@0.83.0/path/mod.ts';
 import { existsSync } from 'https://deno.land/std/fs/mod.ts';
@@ -6,15 +6,19 @@ import '../std/mod.ts'; // Importing Typescript STD by default.
 import '../std/quark.ts';
 
 export function arrayToObject(array: string[][]): Record<string, string> {
-  let result: Record<string, string> = {};
-  for (const item of array) result[item[0]] = item[1];
+  const result: Record<string, string> = {};
+  for (const item of array)
+    result[item[0]] = item[1];
   return result;
 }
 
 export function parseConfiguration(content: string): Record<string, string> {
   try {
     const lines = content.split(/\r?\n/g);
-    const parsed = lines.map((acc) => acc.split('=').map((x) => x.trim()));
+    const parsed = lines.map((acc) => acc
+      .split('=')
+      .map((x) => x.trim())
+    );
     return arrayToObject(parsed);
   } catch (exception) {
     return {};
@@ -22,9 +26,13 @@ export function parseConfiguration(content: string): Record<string, string> {
 }
 
 export async function getQuarkFolder(): Promise<string> {
-  const configuration = !existsSync('.quarkrc') ? {} : parseConfiguration(await File.read('.quarkrc'));
-  const condition = configuration['name'] === 'quark-lang'
-    && Boolean(configuration['core']) === true;
+  const configuration = existsSync('.quarkrc')
+    ? parseConfiguration(await File.read('.quarkrc'))
+    : {};
+
+  const condition =
+    configuration['name'] === 'quark-lang' &&
+    Boolean(configuration['core']) === true;
 
   return !condition
     ? <string>Deno.env.get('QUARK')
