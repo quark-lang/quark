6755f19564f02a960b2051ac09c7bfd955ed3305
thomasvergne <thomas@quark-lang.dev>
2021-03-20 22:31:44 +0100

    🔥 API imports removed.

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { File } from './utils/file.ts';
 import { Interpreter } from './core/interpreter.ts';
 import * as path from 'https://deno.land/std@0.83.0/path/mod.ts';
 import { existsSync } from 'https://deno.land/std/fs/mod.ts';
-import '../std/mod.ts'; // Importing Typescript STD by default.
-import '../std/quark.ts';
 
 export function arrayToObject(array: string[][]): Record<string, string> {
   const result: Record<string, string> = {};
@@ -42,7 +40,7 @@ export async function getQuarkFolder(): Promise<string> {
 async function main(): Promise<void> {
   // Getting sample code content and interpreting it
   const root: string = await getQuarkFolder();
-  const src = path.join(<string>root, 'cli', 'main.qrk');
+  const src = path.join(<string>root, 'sample', 'main.qrk');
   const script: string = await File.read(src);
   await Interpreter.run(script, src);
 }
