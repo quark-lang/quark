90d859040c692f51d584f451a653e8c441f9038c
thomasvergne <thomas@quark-lang.dev>
2021-04-20 01:22:25 +0200

    ✨ Configuration parser error handling added.

--- src/main.ts
+++ src/main.ts
@@ -12,16 +12,17 @@ export function arrayToObject(array: string[][]): Record<string, string> {
 }
 
 export function parseConfiguration(content: string): Record<string, string> {
-  try {
-    const lines = content.split(/\r?\n/g);
-    const parsed = lines.map((acc) => acc
-      .split('=')
-      .map((x) => x.trim())
-    );
-    return arrayToObject(parsed);
-  } catch (exception) {
-    return {};
+  const lines = content.split(/\r?\n/g);
+  const parsed = lines.map((acc) => acc
+    .split('=')
+    .map((x) => x.trim())
+  );
+  const error = parsed.find((x) => x[0].length === 0 || x[1].length === 0);
+  const index = parsed.findIndex((x) => x === error);
+  if (error !== undefined) {
+    throw `Empty value or property at line ${index}: ${lines[index]}`;
   }
+  return arrayToObject(parsed);
 }
 
 export async function getQuarkFolder(): Promise<string> {
@@ -33,16 +34,23 @@ export async function getQuarkFolder(): Promise<string> {
     configuration['name'] === 'quark-lang' &&
     Boolean(configuration['core']) === true;
 
+  const variable = Deno.env.get('QUARK');
+  if (variable === undefined) 
+    throw `You have to export QUARK variable: export QUARK="path/to/quark"`;
   return !condition
-    ? <string>Deno.env.get('QUARK')
+    ? variable
     : '';
 }
 
 async function main(): Promise<void> {
   // Getting sample code content and interpreting it
   const root: string = await getQuarkFolder();
-  const src = path.join(<string>root, 'cli', 'main.qrk');
-  const script: string = await File.read(src);
-  await Interpreter.run(script, src);
+  const src = path.join(root, 'cli', 'main.qrk');
+  try {
+    const script: string = await File.read(src);
+    await Interpreter.run(script, src);
+  } catch(exception) {
+    throw exception;
+  }
 }
 await main();
