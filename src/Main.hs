1c08d7c35465edadfb0227db64fdae28a688894f
thomasvergne <thomas@quark-lang.dev>
2021-05-25 11:03:18 +0200

    🍱 Compilation main script added.

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,13 @@
 {-# OPTIONS_GHC -Wno-deferred-type-errors #-}
 module Main where
   import Core.Parser (parse)
-  import Core.Compiler (compile)
+  import Core.Compiler (compile, initProgram)
+  import Control.Monad.State (runState)
   
   main = do
-    let res = parse "(begin (let t (fn () (print \"test\"))) (t))"
+    let res = parse "(begin (let fun (fn (x) (+ 5 x))) (begin (fun 7) (fun 8)) (let x 5))"
     case res of
-      Left err -> print err
-      Right ast -> print $ compile ast
+      Left err -> error . show $ err
+      Right ast -> do
+        print $ let (_, res) = runState (compile ast) initProgram in res
+        -- print ast
