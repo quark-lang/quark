69f667aa9115155ec422a7597f02450496349312
thomasvergne <thomas@quark-lang.dev>
2021-05-31 23:48:34 +0200

    ✨ Added basic efficiency compilation.

--- src/core/Compiler.hs
+++ src/core/Compiler.hs
@@ -3,13 +3,16 @@ module Core.Compiler where
   import Core.Parser
   import Control.Monad.State
   import Data.List
+  import Data.Sequence (update, fromList)
+  import Data.Foldable (toList)
+
   data Value
     = VString String
     | VInteger Integer
     | VDouble Double
     | VList [Value]
     | VLambda [Bytecode]
-    deriving Show
+    deriving (Show, Eq, Ord)
   data Bytecode
     = PUSH Value
     | STORE String
@@ -18,125 +21,79 @@ module Core.Compiler where
     | PRINT
     | CALL Int
     | DROP String
-    deriving Show
+    deriving (Show, Ord, Eq)
 
   type Page = [Bytecode]
-  type Address = Integer
+  type Address = Int
   type Segment = (Address, Page)
 
   type Variable = String
   type Scope = [Variable]
 
-  type SegmentPointer = Integer
-  type Program = (SegmentPointer, [Segment], [Scope])
-
-  -- Bytecode modifiers section
-  spanBytecode :: State Program (Segment, [Segment]) -- (CurrentEntry, OtherEntries)
-  spanBytecode = do
-    (i,segs,_) <- get
-    entry <- getCurrentEntry
-    let removedEntry = filter ((/=i) . fst) segs
-    return (entry, removedEntry)
+  type Program = (Address, [Page])
 
   push :: Bytecode -> State Program ()
   push instr = do
-    (current,others) <- spanBytecode
-    let pushed = let (addr, bc) = current in (addr, bc ++ [instr])
-    state \(i,_,s) -> ((), (i, others ++ [pushed],s))
+    (address, pages) <- get
+    let page = pages !! address
+    let updated = toList $ update address (page ++ [instr]) $ fromList pages
+    put (address, updated)
 
-  -- Entry setters section
-  newEntry :: State Program ()
-  newEntry = state \(i',entries,s) -> ((), (i', (toInteger $ length entries, []) : entries,s))
+  increment :: State Program ()
+  increment = state \(_,p) -> ((), (length p, p ++ [[]]))
 
-  -- Entry getters section
-  getMainEntry :: State Program Segment
-  getMainEntry = state \p@(_,segs,_) -> (last segs, p)
+  set :: Address -> State Program ()
+  set i = state \(_,p) -> ((), (i, p))
 
-  getCurrentEntry :: State Program Segment
-  getCurrentEntry = state \p@(i,segs,s) -> do
-    case findEntryByPointer i segs of
-      Just x -> (x, p)
-      Nothing -> error $ "Can't load " ++ show i ++ " segment!" ++ show p
+  next :: State Program Address
+  next = state \z@(_,p) -> (length p, z)
 
-  findEntryByPointer :: SegmentPointer -> [Segment] -> Maybe Segment
-  findEntryByPointer i = find ((==i) . fst)
+  address :: State Program Address
+  address = state \z@(i,_) -> (i, z)
 
-  -- Pointer moves section
-  incSegmentPointer :: State Program ()
-  incSegmentPointer = state \(i,p,s) -> ((), (i + 1, p, s))
-
-  decSegmentPointer :: State Program ()
-  decSegmentPointer = state \(i,p,s) -> ((), (i - 1, p,s))
-
-  movSegmentPointer :: SegmentPointer -> State Program ()
-  movSegmentPointer i = state \(_,p,s) -> ((), (i, p,s))
-
-  getCurrentPointer :: State Program SegmentPointer
-  getCurrentPointer = state \p@(i,_,_) -> (i, p)
-
-  -- Compilation section  
-
-  popVariableScope :: State Program Scope
-  popVariableScope = state \(i,p,s) -> (last s, (i, p, init s))
-
-  pushVariable :: String -> State Program ()
-  pushVariable var = state \(i,p,s) -> ((), (i, p, case s of
-    [] -> []
-    _ -> let (head:rest) = s in (var : head) : rest))
-
-  pushNewScope :: State Program ()
-  pushNewScope = state \(i,p,s) -> ((), (i, p, [] : s))
-
-
-  compile :: Atom -> State Program ()
-  compile (Expression (x:xs)) = case x of
-    Word "fn" -> let (args:body:_) = xs in do
-      newEntry
-      incSegmentPointer
+  compile :: Atom ->  State Program ()
+  compile (Expression z@(x:xs)) = case x of
+    Word "fn" -> do
+      let (args:body:_) = xs
+      addr <- address
+      i <- next
+      push $ LOAD_SEGMENT $ toInteger i -- Loading future segment
+      increment -- Incrementing segment pointer
 
       case args of
-        (Expression args') -> mapM_ (\case
-          Word wrd -> push $ STORE wrd
-          _ -> error "Function argument must be a word!")  args'
-        _ -> error "Function arguments must be encapsuled in an atom!"
+        Expression xs -> mapM_ (\case
+          Word x -> push $ STORE x
+          z -> error $ "Argument must be a word, received a " ++ show z) xs
+        _ -> error "Arguments must be an atom!"
 
       compile body
-      index <- getCurrentPointer
-      decSegmentPointer
-      push (LOAD_SEGMENT index)
+      set addr
 
-    Word "begin" -> do
-      pushNewScope
-      mapM_ compile xs
-      variables <- popVariableScope
-      mapM_ (push . DROP) variables
-      
-    Word "print" -> mapM_ compile xs >> push PRINT
     Word "let" -> do
       let (name:value:_) = xs
       compile value
       case name of
-        Word str -> pushVariable str >> push (STORE str)
-        _ -> error "Variable name must be a word !"
+        Word x -> push $ STORE x
+        z -> error $ "Variable name must be a word, received a " ++ show z
     
-    Word "drop" -> let (name:_) = xs in case name of
-      Word wrd -> push $ DROP wrd
-      String str -> push $ DROP str
-      _ -> error "Cannot drop list or integer or double variable!"
+    Word "begin" -> mapM_ compile xs
 
-    Word fn -> do
-      push $ LOAD fn
+    -- Compiling function call
+    Word x -> do
+      push (LOAD x)
       mapM_ compile xs
-      push $ CALL (length xs)
+      push (CALL (length xs))
 
-  compile (String str) = push $ PUSH (VString str)
-  compile (Integer int) = push $ PUSH (VInteger int)
-  compile (Double dbl) = push $ PUSH (VDouble dbl)
-  compile (Word wrd) = push $ LOAD wrd
+    -- Compiling anonymous function
+    Expression z@(Word "fn":_) -> compile x >> mapM_ compile xs >> push (CALL (length xs))
 
+    _ -> mapM_ compile z
+
+  compile (Integer i) = push (PUSH (VInteger i))
+  compile (String s) = push (PUSH (VString s))
   compile _ = return ()
 
   -- Useful functions
 
   initProgram :: Program
-  initProgram = (0, [(0, [])], [])
+  initProgram = (0, [[]]) -- First element represents global scope
