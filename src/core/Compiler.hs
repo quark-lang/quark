40feb9f836f36e75f0e811eabc3ccd372fd838e4
thomasvergne <thomas@quark-lang.dev>
2021-06-01 12:30:32 +0200

    ✨ Added begin and condition compilation.

--- src/core/Compiler.hs
+++ src/core/Compiler.hs
@@ -17,10 +17,11 @@ module Core.Compiler where
     = PUSH Value
     | STORE String
     | LOAD String
-    | LOAD_SEGMENT Integer
+    | LOAD_SEGMENT Int
     | PRINT
     | CALL Int
     | DROP String
+    | JUMP Int Int
     deriving (Show, Ord, Eq)
 
   type Page = [Bytecode]
@@ -30,26 +31,38 @@ module Core.Compiler where
   type Variable = String
   type Scope = [Variable]
 
-  type Program = (Address, [Page])
+  type Program = (Address, [Page], [Scope])
+
+  newScope :: State Program ()
+  newScope = state \(a,p,s) -> ((), (a, p, [] : s))
+
+  variable :: String -> State Program ()
+  variable v = state \(a,p,x:xs) -> ((), (a,p, (x ++ [v]) : xs))
+
+  removeScope :: State Program Scope
+  removeScope = state \(a,p,x:xs) -> (x, (a, p, xs))
+
+  scopes :: State Program [Scope]
+  scopes = state \z@(_,_,s) -> (s, z)
 
   push :: Bytecode -> State Program ()
   push instr = do
-    (address, pages) <- get
+    (address, pages, s) <- get
     let page = pages !! address
     let updated = toList $ update address (page ++ [instr]) $ fromList pages
-    put (address, updated)
+    put (address, updated, s)
 
   increment :: State Program ()
-  increment = state \(_,p) -> ((), (length p, p ++ [[]]))
+  increment = state \(_,p, s) -> ((), (length p, p ++ [[]], s))
 
   set :: Address -> State Program ()
-  set i = state \(_,p) -> ((), (i, p))
+  set i = state \(_,p, s) -> ((), (i, p, s))
 
   next :: State Program Address
-  next = state \z@(_,p) -> (length p, z)
+  next = state \z@(_,p,_) -> (length p, z)
 
   address :: State Program Address
-  address = state \z@(i,_) -> (i, z)
+  address = state \z@(i,_,_) -> (i, z)
 
   compile :: Atom ->  State Program ()
   compile (Expression z@(x:xs)) = case x of
@@ -57,7 +70,7 @@ module Core.Compiler where
       let (args:body:_) = xs
       addr <- address
       i <- next
-      push $ LOAD_SEGMENT $ toInteger i -- Loading future segment
+      push $ LOAD_SEGMENT $ i -- Loading future segment
       increment -- Incrementing segment pointer
 
       case args of
@@ -71,12 +84,33 @@ module Core.Compiler where
 
     Word "let" -> do
       let (name:value:_) = xs
-      compile value
       case name of
-        Word x -> push $ STORE x
+        Word x -> compile value >> push (STORE x) >> variable x
         z -> error $ "Variable name must be a word, received a " ++ show z
-    
-    Word "begin" -> mapM_ compile xs
+
+    Word "begin" -> do
+      newScope
+      mapM_ compile xs
+      scope <- removeScope
+      unless (null scope) $ mapM_ (push . DROP) scope
+
+    Word "if" -> do
+      let (cond:then':else':_) = xs
+      addr <- address
+      compile cond
+
+      thenAddr <- next
+      increment
+      compile then'
+
+      set addr
+
+      elseAddr <- next
+      increment
+      compile else'
+
+      set addr
+      push $ JUMP thenAddr elseAddr
 
     -- Compiling function call
     Word x -> do
@@ -85,15 +119,20 @@ module Core.Compiler where
       push (CALL (length xs))
 
     -- Compiling anonymous function
-    Expression z@(Word "fn":_) -> compile x >> mapM_ compile xs >> push (CALL (length xs))
+    Expression z@(Word "fn":_) -> do
+      compile x 
+      mapM_ compile xs
+      push $ CALL (length xs)
 
     _ -> mapM_ compile z
 
-  compile (Integer i) = push (PUSH (VInteger i))
-  compile (String s) = push (PUSH (VString s))
+  compile (Integer i) = push $ PUSH (VInteger i)
+  compile (String s)  = push $ PUSH (VString s)
+  compile (Double d)  = push $ PUSH (VDouble d)
+  compile (Word w)    = push $ LOAD w 
   compile _ = return ()
 
   -- Useful functions
 
   initProgram :: Program
-  initProgram = (0, [[]]) -- First element represents global scope
+  initProgram = (0, [[]], []) -- First element represents global scope
