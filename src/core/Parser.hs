74cab48d9bd351f3ac952f04ba8272b57cf831d3
thomasvergne <thomas@quark-lang.dev>
2021-06-03 17:24:27 +0200

    ✨ Parser rewritten with Language Parsec.

--- src/core/Parser.hs
+++ src/core/Parser.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE BlockArguments #-}
 module Core.Parser where
-  import Text.ParserCombinators.Parsec hiding (string)
-
-  symbol :: Parser Char
-  symbol = oneOf "!&|~#*+$%-@^<=_/:>?"
+  import Text.ParserCombinators.Parsec hiding (parse)
+  import qualified Text.Parsec
+  import qualified Text.Parsec.Token as P
+  import Text.Parsec.Language
 
   data Atom
     = Word String
@@ -13,55 +13,45 @@ module Core.Parser where
     | String String
     deriving (Read, Show, Eq)
 
-  word :: Parser Atom
-  word = do
-    first_char <- letter <|> symbol
-    rest <- many $ letter <|> digit <|> symbol
-    let atom = first_char : rest
-    pure $ Word atom
+  lexer = P.makeTokenParser def
 
-  escape :: Parser String
-  escape = do
-      d <- char '\\'
-      c <- oneOf "\\\"0nrvtbf"
-      return [d, c]
+  -- Desugaring { instr ... } into (begin instr ...)
+  parseBlock = do
+    instr <- P.braces lexer (many $ parseAtom <|> parseList)
+    pure . Expression $ Word "begin" : instr
 
-  nonEscape :: Parser Char
-  nonEscape = noneOf "\\\"\0\n\r\v\t\b\f"
+  -- Desugaring [i1 i2 ...] into (list i1 i2 ...)
+  parseListBlock = do
+    instr <- P.brackets lexer (many $ parseAtom <|> parseList)
+    pure . Expression $ Word "list" : instr
 
-  character :: Parser String
-  character = fmap return nonEscape <|> escape
+  -- Main parsing entry
+  parseList = Expression <$> 
+        P.parens lexer (many $ parseAtom <|> parseList) 
+    <|> parseBlock
+    <|> parseListBlock
 
-  string :: Parser Atom
-  string = do
-    char '"'
-    x <- many character
-    char '"'
-    pure $ String $ concat x
+  -- Parsing elements in expression
+  parseAtom = choice 
+    [ Word <$> (P.identifier lexer <|> P.operator lexer)
+    , try $ Double <$> P.float lexer
+    , Integer <$> P.integer lexer
+    , String <$> P.stringLiteral lexer
+    ]
 
-  double :: Parser Atom
-  double = do
-    num <- many1 digit
-    char '.'
-    mantissa <- many1 digit
-    pure $ Double . read $  num <> "." <> mantissa
-
-  integer :: Parser Atom
-  integer = do
-    num <- many1 digit
-    pure $ Integer . read $ num
-
-  expression :: Parser Atom
-  expression = Expression <$> sepBy parse' spaces
-
-  parse' :: Parser Atom
-  parse' = word
-       <|> try double
-       <|> integer
-       <|> string
-       <|> do
-            char '('
-            x <- try expression
-            char ')'
-            pure x
-  parse = runParser parse' () ""
+  -- Lisp parser definition
+  def = emptyDef 
+    {
+        P.commentStart   = "/*"
+      , P.commentEnd     = "*/"
+      , P.commentLine    = "#"
+      , P.nestedComments = True
+      , P.identStart     = letter <|> char '_'
+      , P.identLetter    = alphaNum <|> oneOf "_'"
+      , P.opStart        = P.opLetter emptyDef
+      , P.opLetter       = oneOf ":!#$%&*+./<=>?@\\^|-~"
+      , P.reservedOpNames= []
+      , P.reservedNames  = ["import"]
+      , P.caseSensitive  = True
+    }
+  parse = runParser parseList () ""
