185c3bfc00f9fe1ef3727ab94ba760858fe2f086
thomasvergne <thomas@quark-lang.dev>
2021-06-03 17:50:51 +0200

    🐛 Frame call fixed.

--- src/vm/Interpreter.hs
+++ src/vm/Interpreter.hs
@@ -12,7 +12,7 @@ module VM.Interpreter where
 
   revList [] = []
   revList (x:xs) = revList xs ++ [x]
-  
+
   popNTimes :: Int -> StateT Program IO [Compiler.Value]
   popNTimes n = do
     (st, sy) <- get
@@ -23,24 +23,28 @@ module VM.Interpreter where
 
     put (new, sy)
     return args
-  
+
   instruction :: Compiler.Bytecode -> [Compiler.Page] -> StateT Program IO ()
   instruction (Compiler.PUSH v) p = pushValue v
   instruction (Compiler.DROP x) p = void $ popVariable x
+
   instruction (Compiler.LOAD_SEGMENT i) p = do
     let page = p !! i
     f <- frame
     t <- table
     pushValue $ Compiler.VCode page (f, t)
+
   instruction (Compiler.LOAD "*") p = do
     pushValue $ Compiler.VCode [Compiler.MUL] ([], D.dict [])
+
   instruction (Compiler.STORE x) p = do
     val <- popValue
     pushVariable (x, val)
+
   instruction (Compiler.LOAD x) p = do
     (_,val) <- getVariable x
     pushValue val
-  
+
   instruction (Compiler.LIST i) p = do
     elements <- revList <$> popNTimes i
     pushValue $ Compiler.VList elements
@@ -53,7 +57,7 @@ module VM.Interpreter where
 
   instruction Compiler.SUB p = do
     args <- revList <$> popNTimes 2
-    let (a:b:_) = args 
+    let (a:b:_) = args
     pushValue $ case a of
 
       Compiler.VInteger a' -> case b of
@@ -66,51 +70,52 @@ module VM.Interpreter where
 
   instruction Compiler.MUL p = do
     args <- revList <$> popNTimes 2
-    let (a:b:_) = args 
+    let (a:b:_) = args
     pushValue $ case a of
 
       Compiler.VInteger a' -> case b of
         Compiler.VInteger b' -> Compiler.VInteger $ a' * b'
         Compiler.VDouble b' -> Compiler.VDouble $ fromIntegral a' * b'
-        
+
       Compiler.VDouble a' -> case b of
         Compiler.VInteger b' -> Compiler.VDouble $ a' * fromIntegral b'
         Compiler.VDouble b' -> Compiler.VDouble $ a' * b'
 
   instruction Compiler.ADD p = do
     args <- revList <$> popNTimes 2
-    let (a:b:_) = args 
+    let (a:b:_) = args
     pushValue $ case a of
 
       Compiler.VInteger a' -> case b of
         Compiler.VInteger b' -> Compiler.VInteger $ a' + b'
         Compiler.VDouble b' -> Compiler.VDouble $ fromIntegral a' + b'
-        
+
       Compiler.VDouble a' -> case b of
         Compiler.VInteger b' -> Compiler.VDouble $ a' + fromIntegral b'
         Compiler.VDouble b' -> Compiler.VDouble $ a' + b'
 
   instruction (Compiler.CALL x) p = do
     args <- popNTimes x
-    func <- popValue 
-    case func of 
-      Compiler.VCode f _ -> do
-        st <- table
+    func <- popValue
+    case func of
+      Compiler.VCode f (fr, t) -> do
+        (_,ts) <- get
 
         pushTable
-        pushFrame 
+        pushFrame
+        pushValue Compiler.VNone
 
-        mapM_ pushVariable (D.dictToList st)
+        mapM_ pushVariable $ D.dictToList t ++ D.dictToList (last ts)
         mapM_ pushValue args
 
         _ <- return $ unsafePerformIO $ print "test"
 
         mapM_ (`instruction` p) f
-        ret <- popValue 
+        ret <- popValue
 
         popFrame
         popTable
-
+        
         pushValue ret
       _ -> error "Can't call a value as a function!"
 
@@ -118,12 +123,14 @@ module VM.Interpreter where
     a <- popValue
     b <- popValue
     pushValue $ Compiler.VBool (b == a)
+
   instruction (Compiler.JUMP i j) p = do
     a <- popValue
     case a of
       Compiler.VBool x -> let page = p !! (if x then i else j) in
         mapM_ (`instruction` p) page
       _ -> error "Can't jump due to bug!"
+
   instruction _ _ = return ()
 
   run :: [Compiler.Page] -> StateT Program IO ()
