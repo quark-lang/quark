0aa1caada367c4353d9b82ba5e562321127b8cd5
thomasvergne <thomas@quark-lang.dev>
2021-11-10 19:00:59 +0100

    💄 Added pretty printing module.

---
+++ app/Core/Parser/Utils/Pretty.hs
@@ -0,0 +1,44 @@
+module Core.Parser.Utils.Pretty where
+  import Core.Parser.AST (AST(..))
+  import System.Console.ANSI
+  import System.Console.ANSI.Codes (ConsoleIntensity(BoldIntensity))
+  
+  indent :: Integer -> String
+  indent n = replicate (fromIntegral n) ' '
+
+  showAST :: Integer -> AST -> IO ()
+  showAST i (Node n xs) = do
+    putStr . indent $ i
+    setSGR [SetColor Foreground Dull Green]
+    putStr "Node "
+    setSGR [Reset, SetConsoleIntensity BoldIntensity]
+    putStrLn n
+    setSGR [Reset]
+    mapM_ (showAST (i + 2)) xs
+  showAST i val = 
+    putStr (indent i) >> case val of
+     Integer n -> do
+       setSGR [SetColor Foreground Vivid Black]
+       putStr "Integer "
+       setSGR [Reset, SetColor Foreground Vivid Yellow]
+       print n
+       setSGR [Reset]
+     String s -> do
+       setSGR [SetColor Foreground Vivid Black]
+       putStr "String "
+       setSGR [Reset, SetColor Foreground Dull Green]
+       print s
+       setSGR [Reset]
+     Float x -> do
+       setSGR [SetColor Foreground Vivid Black]
+       putStr "Float "
+       setSGR [Reset, SetColor Foreground Vivid Yellow]
+       print x
+       setSGR [Reset]
+     Literal s -> do
+        setSGR [SetColor Foreground Vivid Black]
+        putStr "Literal "
+        setSGR [Reset, SetConsoleIntensity BoldIntensity]
+        putStrLn s
+        setSGR [Reset]
+     _ -> print "NO CASE"
