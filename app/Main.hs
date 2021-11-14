60626e2538fe166a55dbfc2d1fd6ef592e34ce7c
NessMC <contact@nessmc.fr>
2021-11-14 17:42:10 +0100

    🍱 Added closure imports.

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
-  import Core.Parser.Utils.Module (parse, garbageCollection, removeOne)
+  import Core.Parser.Utils.Module (parse)
   import Core.Parser.Utils.Pretty (showAST)
+  import Core.Parser.Utils.Garbage (garbageCollection)
+  import Core.Parser.Utils.Closure (convertClosure)
   
   main :: IO ()
   main = do
@@ -11,5 +13,5 @@ module Main where
       Nothing -> print "ERROR"
       Just ast -> do
         let r = garbageCollection ast
-        showAST 0 r
+        showAST 0 (convertClosure r r)
 
