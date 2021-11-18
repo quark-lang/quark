498d52365047be8a13642a658aaa4bc17d6438ed
thomasvergne <thomas@quark-lang.dev>
2021-11-18 22:12:32 +0100

    🔧 FFI calls added in main.

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,35 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE CApiFFI #-}
 module Main where
   import Core.Parser.Utils.Module (parse)
   import Core.Parser.Utils.Pretty (showAST)
   import Core.Parser.Utils.Garbage (garbageCollection)
   import Core.Parser.Utils.Closure (convertClosure, Data(..))
   import Core.Compiler.Compiler (compile)
-  import Core.Compiler.Utils.Pretty
-  
-  
+  import Core.Compiler.Utils.Pretty (showBytecode)
+  import Core.Compiler.Serializer
+
+  import Foreign.C.Types
+  import Foreign.Ptr
+  import Foreign.StablePtr ()
+  import Foreign.C.String
+  import Foreign.Marshal.Array
+  import Core.Parser.Utils.ConstantPropagation
+  foreign import capi "Core/VM/main.c runVM"
+    runVM :: Ptr (Ptr CInt) -> CInt -> Ptr (Ptr CChar) -> CInt -> IO CInt
+
+  convertBlob :: [[Int]] -> IO (Ptr (Ptr CInt))
+  convertBlob xs = do
+    ys <- mapM (\x -> do
+      newArray (map (CInt . fromIntegral) x)) xs
+    newArray ys
+
+  convertConstants :: [String] -> IO (Ptr (Ptr CChar))
+  convertConstants xs = do
+    ys <- mapM newCString xs
+    newArray ys
+
   main :: IO ()
   main = do
     let src = "tests/path.qrk"
@@ -18,8 +40,16 @@ module Main where
         let r = garbageCollection ast
         let d = Data []
         let c = convertClosure (r, d) r
-        showAST 0 c
+        -- showAST 0 c
         let res = compile 0 c
         showBytecode res
+        let Serialized blob constants = runSerializer res
+        print blob
 
+        blob' <- convertBlob blob
+        constants' <- convertConstants constants
 
+        x <- runVM blob' (fromIntegral $ length blob) constants' (fromIntegral $ length constants)
+        print x
+
+    
