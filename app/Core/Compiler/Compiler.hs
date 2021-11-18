d857dba11d42bfa3d9fc95f213e78bafe9402162
thomasvergne <thomas@quark-lang.dev>
2021-11-18 21:01:12 +0100

    🐛 Fixed compiler.

--- app/Core/Compiler/Compiler.hs
+++ app/Core/Compiler/Compiler.hs
@@ -3,8 +3,8 @@ module Core.Compiler.Compiler where
   import Core.Parser.AST (AST(..))
   import Core.Parser.Utils.Garbage (toList)
 
-  flat :: [[a]] -> [a] -> [a]
-  flat xs acc = foldl (++) acc xs
+  flat :: [[a]] -> [a]
+  flat xs = foldl (++) [] xs
 
   compile :: Int -> AST -> Bytecode
   compile i (Node (Literal "begin") xs)
@@ -29,33 +29,35 @@ module Core.Compiler.Compiler where
 
   compile i (Node (Literal "return") (x:_))
     = compile i x ++ [RETURN]
-  
+
   compile i (Node (Literal "add") (x:y:_))
     = compile i x ++ compile i y ++ [ADD]
-  
+
   compile i (Node (Literal "sub") (x:y:_))
     = compile i x ++ compile i y ++ [SUB]
 
   compile i (Node (Literal "mul") (x:y:_))
     = compile i x ++ compile i y ++ [MUL]
-  
+
   compile i (Node (Literal "div") (x:y:_))
     = compile i x ++ compile i y ++ [DIV]
 
   compile i (Node (Literal "if") (cond:then_:else_:_))
     = let cond' = compile (i + 1) cond
+          i' = i + 1
           lc = length cond'
 
-          then_' = compile (i + lc + 1) then_
+          -- taking in account the condition length to compute new i
+          then_' = compile (i' + lc) then_
           lt = length then_'
 
-          else_' = compile (i + lt + lc + 1) else_
+          else_' = compile (i' + lt + lc + 1) else_
           le = length else_'
 
-          i' = i + 1
-      in cond' ++ [JUMP_IF (lc + i') (lc + i' + lt + 1)] 
-          ++ then_' ++ [JUMP (lc + i' + lt + le + 2)] 
-          ++ else_' ++ [JUMP (lc + i' + lt + le + 2)]
+          -- computing new i with 1 for the incrementing and two for the jump
+      in cond' ++ [JUMP_ELSE $ i' + lt + lc + 1]
+          ++ then_' ++ [JUMP_REL $ le + 1]
+          ++ else_'
 
   compile i (Node (Literal "fn") (args:body))
     = let args'  = map (\(Literal name) -> STORE name) (toList args)
@@ -69,4 +71,8 @@ module Core.Compiler.Compiler where
 
   compile i (Literal x) = [LOAD x]
   compile _ (Integer i) = [PUSH (fromInteger i)]
+  compile _ (Float f)   = [PUSH f]
   compile _ _ = []
+
+  serialize :: Bytecode -> (String, Int)
+  serialize xs = (flat $ map ((++";") . show) xs, length xs)
