8a49ba19b4cc65972b8b5de85d562b9de11daafd
NessMC <contact@nessmc.fr>
2021-12-12 17:02:49 +0100

    🐛 Fixed condition compilation.

--- app/Core/Compiler/Compiler.hs
+++ app/Core/Compiler/Compiler.hs
@@ -26,20 +26,23 @@ module Core.Compiler.Compiler where
 
   compile i (Node (Literal "chr") (x:_))
     = compile i x ++ [EXTERN 2]
+  
+  compile i (Node (Literal "=") (x:y:_))
+    = compile i x ++ compile i y ++ [EXTERN 1]
 
   compile i (Node (Literal "return") (x:_))
     = compile i x ++ [RETURN]
 
-  compile i (Node (Literal "add") (x:y:_))
+  compile i (Node (Literal "+") (x:y:_))
     = compile i x ++ compile i y ++ [ADD]
 
-  compile i (Node (Literal "sub") (x:y:_))
+  compile i (Node (Literal "-") (x:y:_))
     = compile i x ++ compile i y ++ [SUB]
 
-  compile i (Node (Literal "mul") (x:y:_))
+  compile i (Node (Literal "*") (x:y:_))
     = compile i x ++ compile i y ++ [MUL]
 
-  compile i (Node (Literal "div") (x:y:_))
+  compile i (Node (Literal "/") (x:y:_))
     = compile i x ++ compile i y ++ [DIV]
 
   compile i (Node (Literal "if") (cond:then_:else_:_))
@@ -55,8 +58,8 @@ module Core.Compiler.Compiler where
           le = length else_'
 
           -- computing new i with 1 for the incrementing and two for the jump
-      in cond' ++ [JUMP_ELSE $ i' + lt + lc + 1]
-          ++ then_' ++ [JUMP_REL $ le + 1]
+      in cond' ++ [JUMP_ELSE $ lt + 1]
+          ++ then_' ++ [JUMP_REL le]
           ++ else_'
 
   compile i (Node (Literal "fn") (args:body))
@@ -64,11 +67,15 @@ module Core.Compiler.Compiler where
           body'  = map (compile i) body
           body'' = concat (args' : body')
           l      = length body''
-      in  MAKE_LAMBDA l : body''
+      in  MAKE_LAMBDA (l + 1) : body'' ++ [RETURN]
 
   compile i (Node x xs)
     = foldl (\acc x -> acc ++ compile (length acc + i) x) (compile i x) xs ++ [CALL (length xs)]
 
+  compile _ (Literal "+") = [ADD]
+  compile _ (Literal "-") = [SUB]
+  compile _ (Literal "*") = [MUL]
+  compile _ (Literal "/") = [DIV]
   compile i (Literal x) = [LOAD x]
   compile _ (Integer i) = [PUSH (fromInteger i)]
   compile _ _ = []
