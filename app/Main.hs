f8e8b66453a6834f87bab821b3e27262d2d1f412
Thomas <thomas.vergne.contact@gmail.com>
2021-12-23 16:41:06 +0100

    🔧 Added closure conversion imports.

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,11 @@ module Main where
   import Core.Parser.Utils.Module (parse)
   import Core.Parser.Utils.Pretty (showAST)
   import Core.Parser.Utils.Garbage (runGarbageCollector)
-  import Core.Parser.Utils.ConstantPropagation ( propagate )
+  import Core.Parser.Utils.ConstantPropagation (propagate, runRemover)
   import Core.Parser.Macros (runMacroCompiler)
+  import Core.Parser.Utils.ClosureConversion
+  import Core.Compiler.Compiler
+  import Core.Compiler.Javascript (compileJavascript)
   
   main :: IO ()
   main = do
@@ -13,8 +16,11 @@ module Main where
       Nothing -> print "ERROR"
       Just ast -> do
         m <- runMacroCompiler ast
-        let c = propagate m
-        g <- runGarbageCollector c
-        showAST 0 g
+        g <- runGarbageCollector m
+        let c = runRemover $ propagate g
+        showAST 0 c
+        ClosureConversion _ ast _ _ <- runClosureConverter c
+        mapM_ (\(Closure n _ (t, b)) -> print (n, t, b)) ast
+        
 
     
