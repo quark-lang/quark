a6da81f80826996c11824833c85e78a9b4affb02
Thomas <thomas.vergne.contact@gmail.com>
2021-12-24 00:33:37 +0100

    ✨ Started writing basic compiler.

--- app/Core/Compiler/Compiler.hs
+++ app/Core/Compiler/Compiler.hs
@@ -1,99 +1,91 @@
 {-# LANGUAGE ConstraintKinds, FlexibleContexts #-}
 {-# LANGUAGE LambdaCase #-}
 module Core.Compiler.Compiler where
-  import qualified Core.Parser.AST as A
+  import Core.Parser.AST
   import Control.Monad.State
   import GHC.Float (float2Double)
   import Core.Parser.Macros (unliteral)
   import Data.Functor ((<&>))
+  import Core.Parser.Utils.ClosureConversion
+  import Core.Compiler.Instruction
+  import Control.Monad.State (evalStateT)
 
-  data CompilerState = CompilerState {
-    bound :: [(String, String)],
-    stateID :: Int
+  data Lambda = Lambda {
+    pointer :: Int,
+    variables :: [(String, Int)]
   } deriving Show
 
-  type Compiler m = (MonadState CompilerState m, MonadIO m)
+  type LambdaState m = (MonadState Lambda m, MonadIO m)
 
-  -- Representing a Javascript AST
-  data AST
-    -- Expressions
-    = FunctionCall AST [AST]
-    | Identifier String
-    | String String
-    | Number Double
-    | Lambda [String] AST
-    | BinaryExpression String AST AST
-    | Block [AST]
+  addPointer :: LambdaState m => String -> m Int
+  addPointer n = do
+    Lambda p vars <- get
+    put $ Lambda (p + 1) ((n, p) : vars)
+    return p
 
-    -- Statements
-    | Assignment String AST
-    | Condition AST AST AST
-    | Delete AST
-    deriving Show
+  getPointer :: LambdaState m => String -> m Int
+  getPointer n = do
+    Lambda p vars <- get
+    case lookup n vars of
+      Just p' -> return p
+      Nothing -> liftIO (print n) >> return (-1)
 
-  addVariable :: Compiler m => String -> m (String, String)
-  addVariable name = do
-    bound <- gets bound
-    id <- gets stateID
-    modify $ \s -> s { stateID = id + 1, bound = (name, "_v" ++ show id) : bound }
-    return (name, "_v" ++ show id)
+  toInt :: Float -> Int
+  toInt = round
 
-  popVariable :: Compiler m => m (String, String)
-  popVariable =
-    gets bound >>= \case
-      [] -> error "No variables to pop"
-      (x:xs) -> do
-        modify $ \s -> s { bound = xs }
-        return x
+  startsWith :: String -> String -> Bool
+  startsWith a b = a == take (length a) b
 
-  compile :: Compiler m => A.AST -> m AST
-  compile (A.Node (A.Literal "begin") xs) = do
-    before <- gets stateID
-    xs' <- mapM compile xs
-    after  <- gets stateID
-    replicateM_ (after - before) popVariable
-    return $ Block xs'
+  removeStringFromString :: String -> String -> String
+  removeStringFromString a = drop (length a)
 
-  compile (A.Node (A.Literal "let") [A.Literal name, value]) = do
-    (_, id) <- addVariable name
-    return <$> Assignment id =<< compile value
+  compileLambda :: LambdaState m => ClosuredNode -> Int -> m Section
+  compileLambda (Closure name args (env, body)) i = do
+    mapM_ addPointer $ args ++ env
+    Section i <$> helper body
+    where helper :: LambdaState m => AST -> m [Instruction]
+          helper (Node (Literal "let") [Literal name, value]) = do
+            addr <- addPointer name
+            v <- helper value
+            return $ v ++ [STORE addr]
+          helper (Node (Literal "begin") xs) = concat <$> mapM helper xs
+          helper (Node (Literal "print") [value]) = helper value <&> (++ [EXTERN 0])
+          helper (Node (Literal "input") [value]) = helper value <&> (++ [EXTERN 1])
+          helper (Node (Literal "=") [lhs, rhs]) = do
+            lhs' <- helper lhs
+            rhs' <- helper rhs
+            return $ lhs' ++ rhs' ++ [EXTERN 2]
+          helper (Node (Literal "-") [lhs, rhs]) = do
+            lhs' <- helper lhs
+            rhs' <- helper rhs
+            return $ lhs' ++ rhs' ++ [EXTERN 3]
+          helper (Node (Literal "if") [cond, t, e]) = do
+            c <- helper cond
+            t' <- helper t
+            e' <- helper e
+            return $ c ++ [JUMP_ELSE (length t')] ++ t' ++ [JUMP_REL (length e')] ++ e'
+          helper (Node (Literal "drop") [Literal n]) =
+            if startsWith "lambda" n
+              then return []
+              else do
+                addr <- getPointer n
+                return [DROP addr]
+            
+          helper (Node n xs) = do
+            n' <- helper n
+            xs' <- mapM helper xs
+            return $ n' ++ concat xs' ++ [CALL (length xs)]
+          helper (Literal n) =
+            if startsWith "lambda" n
+              then return [LOAD_SECTION . read $ removeStringFromString "lambda" n]
+              else do
+                addr <- getPointer n
+                return [LOAD addr]
+          helper (Float f)   = return [PUSH $ toInt f]
+          helper (Integer n) = return [PUSH $ fromInteger n]
+          helper x = return []
 
-  compile (A.Node (A.Literal "fn") [A.Node (A.Literal "list") args, body]) = do
-    before <- gets stateID
-    args' <- mapM (\(A.Literal name) -> addVariable name <&> snd) args
-    xs <- compile body
-    after <- gets stateID
-    replicateM_ (after - before) popVariable
-    return $ Lambda args' xs
-
-  compile (A.Node (A.Literal "if") [cond, then_, else_]) =
-    Condition <$> compile cond <*> compile then_ <*> compile else_
-
-  compile (A.Node (A.Literal "drop") [name]) = Delete <$> compile name
-
-  compile (A.Node n xs) =
-    compile n >>= \case
-      x@(Identifier i) -> if i `elem` binaryExprs
-        then BinaryExpression i <$> compile (head xs) <*> compile (last xs)
-        else FunctionCall x <$> mapM compile xs
-      x -> FunctionCall x <$> mapM compile xs
-
-  compile (A.Literal "nil") = return $ Identifier "null"
-
-  compile (A.Literal s) = do
-    bound <- gets bound
-    return $ case lookup s bound of
-      Just id -> Identifier id
-      Nothing -> Identifier s
-
-  compile (A.Float n) = return . Number . float2Double $ n
-  compile (A.String s) = return $ String s
-  compile (A.Integer n) = return . Number . fromIntegral $ n
-
-  compile n = liftIO $ print n >> return (Block [])
-  
-  binaryExprs :: [String]
-  binaryExprs = ["+", "-", "*", "/", ">", "=", "or", "and"]
-
-  runCompiler :: MonadIO m => A.AST -> m AST
-  runCompiler a = evalStateT (compile a) (CompilerState [("print", "console.log")] 0)
+  compile :: (Monad m, MonadIO m) => ClosuredAST -> m [Section]
+  compile xs = do
+    let data' = zip [0..] xs
+      in mapM (\(i, x) -> evalStateT (compileLambda x i) (Lambda 0 [])) data'
