00605e26b9c693e1428107f819bf063eb7fedde2
NessMC <contact@nessmc.fr>
2022-04-20 14:42:09 +0200

    🚧 Started kind-checking.

--- app/Core/Inference/Kind.hs
+++ app/Core/Inference/Kind.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 module Core.Inference.Kind where
   import qualified Data.Map as M
-  import Control.Monad.State (MonadIO, modify, MonadState(get))
+  import Control.Monad.State (MonadIO, modify, MonadState(get), liftIO)
   import Control.Monad.RWS (MonadRWS, MonadReader(local, ask))
   import qualified Core.Parser.AST as A
 
@@ -45,7 +45,9 @@ module Core.Inference.Kind where
     kyFree (k1 :~> k2) = kyFree k1 ++ kyFree k2
     kyFree (KVar k) = [k]
 
-    kyApply sub (KVar k) = M.findWithDefault (KVar k) k sub
+    kyApply sub (KVar k) = case M.lookup k sub of
+      Just t -> kyApply sub t
+      Nothing -> KVar k
     kyApply sub (k1 :~> k2) = kyApply sub k1 :~> kyApply sub k2
     kyApply _   Star = Star
 
@@ -56,7 +58,7 @@ module Core.Inference.Kind where
           s2 = kyUnify (kyApply s1 r) (kyApply s1 r')
         in s1 `kyCompose` s2
     kyUnify Star Star = M.empty
-    kyUnify _ _ = error "kinds do not unify"
+    kyUnify k1 k2 = error $ "Kind " ++ show k1 ++ " and " ++ show k2 ++ " cannot be unified"
   instance Kinds a => Kinds [a] where
     kyFree = concatMap kyFree
     kyApply = map . kyApply
@@ -66,5 +68,35 @@ module Core.Inference.Kind where
     kyApply = M.map . kyApply
     kyUnify = error "kinds do not unify"
 
-  kyInfer :: MonadKind m => A.AST -> m (SubKy, Kind)
-  kyInfer _ = error "Not implemented yet"
+  buildKindFun :: [Kind] -> Kind
+  buildKindFun = foldr1 (:~>)
+
+  removeNArguments :: Int -> Kind -> Kind
+  removeNArguments n (k1 :~> k2) = removeNArguments (n - 1) k2
+  removeNArguments 0 k = k 
+  removeNArguments n _ = error "removeNArguments: not enough arguments"
+
+  kyCheck :: MonadKind m => A.AST -> m (SubKy, Kind)
+  kyCheck (A.Literal "*") = return (M.empty, Star)
+  kyCheck (A.Literal "str") = return (M.empty, Star)
+  kyCheck (A.Literal "int") = return (M.empty, Star)
+  kyCheck (A.Literal n) = ask >>= \env -> case M.lookup n env of
+    Just t -> return (M.empty, t)
+    Nothing -> error $ "unknown type: " ++ show n
+  kyCheck (A.Node (A.Literal "->") xs) = do
+    r <- mapM kyCheck xs
+    let es = merge (map fst r)
+        ts = map snd r
+    return (es, kyApply es $ buildKindFun ts)
+  kyCheck (A.Node n xs) = do
+    (e, kn) <- kyCheck n
+    r <- mapM (local (kyApply e) . kyCheck) xs
+    let es   = merge (map fst r)
+        ts   = map snd r
+        env  = e `kyCompose` es
+        fun  = buildKindFun (ts ++ [Star])
+        uni  = kyUnify fun kn
+        env' = uni `kyCompose` env
+    liftIO $ print (n, fun)
+    return (env', removeNArguments (length r) $ kyApply env' fun)
+  kyCheck x = error $ "unknown type: " ++ show x
