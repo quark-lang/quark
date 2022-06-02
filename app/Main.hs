4be8969c58da26bb4b117537b98dde949a752175
thomasvergne <thomas.vergne.contact@gmail.com>
2022-06-02 21:12:36 +0200

    ✨ Added processing steps.

--- app/Main.hs
+++ app/Main.hs
@@ -3,14 +3,14 @@ module Main where
   import Core.Parser.Utils.Garbage (runGarbageCollector)
   import Core.Parser.Utils.ConstantPropagation (propagate, runRemover)
   import Core.Parser.Macros (runMacroCompiler)
-  --import Core.Parser.Utils.ClosureConversion -- (runConverter, closures)
+  import Core.Parser.Utils.ClosureConversion -- (runConverter, closures)
   --import Core.Compiler.CLang (runCompiler, outputC)
   import Core.Inference.Type (runInfer)
 
   import System.Environment (getArgs)
   import System.Directory (getCurrentDirectory)
   import System.FilePath ((</>))
-  
+
   main :: IO ()
   main = do
     (file:_) <- getArgs
@@ -20,17 +20,14 @@ module Main where
     case res of
       Nothing -> print "ERROR"
       Just ast -> do
-        --m <- runMacroCompiler ast
-        -- removing useless scope related things
-        --g <- runGarbageCollector m
+        m <- runMacroCompiler ast
         -- propagating constants and removing useless code
-        --let r = runRemover $ propagate g
+        let r = runRemover $ propagate m
         -- creating a typed AST
-        -- converting closures
-        -- print ast
-        t <- runInfer ast
+        t <- runInfer r
         print t
-        --c <- convertClosures t
-        --mapM_ print c
+        (closures, ast) <- unzip <$> mapM convertClosures t
+
+        print ast
         -- c <- runCompiler $ closures t
         -- writeFile (dir </> (file -<.> "c")) (outputC c)
