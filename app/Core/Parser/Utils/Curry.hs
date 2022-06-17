75140d3899e4079701065e8ff9faa12d7ee6cb39
thomasvergne <thomas.vergne.contact@gmail.com>
2022-06-17 22:37:22 +0200

    ✨ Added natural list constructor building.

--- app/Core/Parser/Utils/Curry.hs
+++ app/Core/Parser/Utils/Curry.hs
@@ -16,13 +16,21 @@ module Core.Parser.Utils.Curry where
     "->", "match", "data", "if"
     ]
 
+  buildList :: [AST] -> AST
+  buildList [] = Node (Literal "Nil") []
+  buildList (x:xs) = Node (Literal "Cons") [x, buildList xs]
+
   curry :: AST -> AST
   curry (Node (Literal "fn") (List args:body:_)) = buildClosure args $ curry body
   curry (Node (Literal "begin") [List xs]) = Node (Literal "begin") [List $ map curry xs]
   curry z@(Node (Literal "declare") _) = z
+  curry (Node (Literal "match") (x:pats))
+    = let x'    = curry x
+          pats' = (map (\(List [pat, b]) -> List [curry pat, curry b])) pats
+        in Node (Literal "match") (x':pats')
   curry z@(Node (Literal "data") [_, _]) = z
   curry (Node n xs)
     = let xs' = map curry xs
         in if n `elem` reserved then Node n xs' else buildCall (curry n) xs'
-  curry (List xs) = List $ map curry xs
+  curry (List xs) = curry $ buildList xs
   curry z = z
