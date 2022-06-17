218013703611bb1928cfeffd6662b2d22f2563dc
thomasvergne <thomas.vergne.contact@gmail.com>
2022-06-17 23:29:42 +0200

    💄 Added basic AST prettier.

--- app/Core/Parser/AST.hs
+++ app/Core/Parser/AST.hs
@@ -1,4 +1,6 @@
 module Core.Parser.AST where
+  import Data.List
+  import Core.Color
   data AST
     = Node AST [AST]
     | Integer Integer
@@ -7,4 +9,13 @@ module Core.Parser.AST where
     | Literal String
     | Char Char
     | List [AST]
-    deriving (Show, Eq)
+    deriving Eq
+
+  instance Show AST where
+    show (Node a b) = "(" ++ show a ++ " " ++ intercalate " " (map show b) ++ ")"
+    show (Integer i) = bYellow $ show i
+    show (String s) = bGreen $ show s
+    show (Float f) = bYellow $ show f
+    show (Literal s) = bBlue s
+    show (Char c) = bGreen $ show c
+    show (List l) = bBlack "[" ++  intercalate " " (map show l) ++ bBlack "]"
