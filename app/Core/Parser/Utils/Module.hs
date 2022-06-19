a49d18ef7717545c9f28662668054982c1b4ca7b
thomasvergne <thomas.vergne.contact@gmail.com>
2022-06-19 22:53:50 +0200

    ✨ Added STD import path prefix.

--- app/Core/Parser/Utils/Module.hs
+++ app/Core/Parser/Utils/Module.hs
@@ -2,22 +2,27 @@
 module Core.Parser.Utils.Module where
   import qualified Core.Parser.Parser as Parser
   import Core.Parser.AST        (AST(..))
-  import System.Directory       (doesFileExist)
-  import System.FilePath        ((</>), dropFileName)
-  import Core.Parser.Combinator (runParser, many)
+  import System.Directory
+  import System.FilePath
   import Data.Functor           ((<&>))
   import Data.Foldable          (foldlM)
   import Data.Char              (ord)
-  import System.Directory
   import System.Process
-  import System.FilePath ((</>), (-<.>))
   import Core.Color
-
+  import Data.List  
+  import System.Environment (lookupEnv)
+  
   {-
     Module: Parser utils
     Description: Set of functions to extra-parse AST 
     Author: thomasvergne
   -}
+
+  startsWith :: String -> String -> Bool
+  startsWith p s = p `isPrefixOf` s
+
+  endsWith :: String -> String -> Bool
+  endsWith p s = p `isSuffixOf` s
 
   parse :: String -> IO (Maybe AST)
   parse file = do
@@ -25,7 +30,7 @@ module Core.Parser.Utils.Module where
     if exists
       then do
         content <- Parser.format <$> readFile file
-        let (res, _) = runParser (many Parser.parse) content
+        let res = Parser.parseLisp content
         case res of
           Left err -> print err >> return Nothing
           Right ast ->
@@ -35,8 +40,19 @@ module Core.Parser.Utils.Module where
 
   visitAST :: String -> AST -> IO AST
   -- resolving import
-  visitAST p z@(Node (Literal "import") [String path]) =
-    return $ Node (Literal "import") [String $ p </> path]
+  visitAST p z@(Node (Literal "import") [String path]) = do
+    let path' = path ++ if endsWith ".qrk" path then "" else ".qrk"
+    path <- if startsWith "std:" path'
+      then lookupEnv "QUARK" >>= \case
+        Just x -> do
+          let path'' = x </> "tests" </> drop 4 path' 
+          exists <- doesFileExist path''
+          if exists
+            then return path''
+            else error $ "Directory " ++ path'' ++ " does not exist"
+        Nothing -> error "QUARK environment variable is not set"
+      else return $ p </> path'
+    return $ Node (Literal "import") [String path]
 
   visitAST p z@(Node (Literal "begin") [List xs]) = do
     xs <- mapM (visitAST p) xs
