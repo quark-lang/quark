dd8c548874c82b35b0b541a4058462db10f9c7b1
thomasvergne <thomas.vergne.contact@gmail.com>
2022-06-21 21:44:16 +0200

    ✨ Added special character compilation.

--- app/Core/Compiler/Javascript.hs
+++ app/Core/Compiler/Javascript.hs
@@ -32,9 +32,8 @@ module Core.Compiler.Javascript where
   encodeUnicode16 = concatMap escapeChar
     where
       escapeChar c
-          | ' ' <= c && c <= 'z' = [c]
-          | otherwise =
-              printf "\\u%04x" (fromEnum c)
+        | ' ' <= c && c <= 'z' = [c]
+        | otherwise = printf "\\u%04x" (fromEnum c)
 
 
   data Expression
@@ -57,6 +56,7 @@ module Core.Compiler.Javascript where
     | Block [Expression]
     | Throw Expression
     | Require String
+    | Raw String
     deriving (Show, Eq)
 
   from :: Expression -> String
@@ -81,6 +81,7 @@ module Core.Compiler.Javascript where
   from (Lit (F f)) = show f
   from (Lit (I i)) = show i
   from (Require p) = "Object.entries(require(\"" ++ p ++ "\")).map(([name, exported]) => global[name] = exported);"
+  from (Raw c) = c
   from _ = error "from: not implemented"
 
   addCons :: MonadCompiler m => (String, String) -> m ()
@@ -89,14 +90,14 @@ module Core.Compiler.Javascript where
   compileData :: MonadCompiler m => UncurriedAST -> m Expression
   compileData (DataE (n, _) cons) = do
     cons' <- mapM (\(n', t) -> do
-              addCons (n', n)
+              addCons (varify n', varify n)
               case t of
                 args :-> _ -> do
                   let args' = zipWith const (["v" ++ show i | i <- [0..]]) args
                   let fields = map (\x -> (x, Var x)) args'
-                  return (n', Lambda args' (Object (("type", Lit (S n')) : fields)))
-                _ -> return (n', Object [("type", Lit (S n'))])) cons
-    return $ Let n (Object cons')
+                  return (varify n', Lambda args' (Object (("type", Lit (S n')) : fields)))
+                _ -> return (varify n', Object [("type", Lit (S n'))])) cons
+    return $ Let (varify n) (Object cons')
   compileData _ = error "compileData: not a data"
 
   type Variables    = [String]
@@ -108,14 +109,14 @@ module Core.Compiler.Javascript where
 
   compileCase :: MonadCompiler m => UncurriedPattern -> m (Expression -> Expression -> Expression)
   compileCase (VarP n _) = do
-    get >>= \e -> case M.lookup n e of
+    get >>= \e -> case M.lookup (varify n) e of
       Just _ -> return $ \x body ->
         let cond = BinaryCall (Property x (Var "type")) "===" (Lit (S n))
           in Condition cond $ Return body
       Nothing -> return $ \x body -> Block [Let n x, Return body]
   compileCase (AppP (n, _) args _) = do
     let args' = zipWith (curry (\case
-                  (VarP n _, i) -> (Just $ \x -> Let n $ Property x (Var i), Nothing)
+                  (VarP n _, i) -> (Just $ \x -> Let (varify n) $ Property x (Var i), Nothing)
                   (LitP l _, i) -> (Nothing, Just $ \x ->
                     BinaryCall (Property x (Var i)) "===" (Lit l))
                   (WilP _, i) -> (Nothing, Nothing)
@@ -144,6 +145,9 @@ module Core.Compiler.Javascript where
       return $ p x b) pats
   compilePattern _ = error "compilePattern: not a pattern"
 
+  varify :: String -> String
+  varify x = "$" ++ concatMap (\x -> if isLetter x then [x] else show (ord x)) x
+
   compile :: MonadCompiler m => UncurriedAST -> m Expression
   -- JS AST Generation
   compile (AppE ("Call", _) [n] _) = Call <$> compile n <*> pure []
@@ -153,6 +157,7 @@ module Core.Compiler.Javascript where
   compile (AppE ("Throw", _) [x] _) = Throw <$> compile x
   compile (AppE ("Block", _) xs _) = Block <$> mapM compile xs
   compile (AppE ("require", _) [LitE (S path) _] _) = return $ Require path
+  compile (AppE ("from_javascript", _) [LitE (S content) _] _) = return $ Raw content
 
   -- Binary calls
   compile (AppE ("*", _) [x, y] _) = BinaryCall <$> compile x <*> pure "*" <*> compile y
@@ -161,20 +166,20 @@ module Core.Compiler.Javascript where
   compile (AppE ("+", _) [x, y] _) = BinaryCall <$> compile x <*> pure "+" <*> compile y
   compile (AppE ("/", _) [x, y] _) = BinaryCall <$> compile x <*> pure "/" <*> compile y
 
-  compile (AppE (n, _) args _) = get >>= \e -> case M.lookup n e of
+  compile (AppE (n, _) args _) = get >>= \e -> case M.lookup (varify n) e of
     -- Checking if it's a constructor
-    Just obj -> Call (Property (Var obj) (Var n)) <$> mapM compile args
-    Nothing -> Call (Var n) <$> mapM compile args
-  compile (AbsE args body) = Lambda (map fst args) <$> compile body
-  compile (VarE t _) = get >>= \e -> case M.lookup t e of
-    Just obj -> return $ Property (Var obj) (Var t)
-    Nothing -> return $ Var t
+    Just obj -> Call (Property (Var obj) (Var $ varify n)) <$> mapM compile args
+    Nothing -> Call (Var $ varify n) <$> mapM compile args
+  compile (AbsE args body) = Lambda (map (varify . fst) args) <$> compile body
+  compile (VarE t _) = get >>= \e -> case M.lookup (varify t) e of
+    Just obj -> return $ Property (Var obj) (Var $ varify t)
+    Nothing -> return . Var . varify $ t
   compile (LetInE (n, _) value expr) = do
     value <- compile value
     expr  <- compile expr
-    return $ Call (Lambda [n] expr) [value]
+    return $ Call (Lambda [varify n] expr) [value]
   compile (ListE exprs _) = Array <$> mapM compile exprs
-  compile (LetE (n, _) value) = Let n <$> compile value
+  compile (LetE (n, _) value) = Let (varify n) <$> compile value
   compile (LitE l _) = return $ Lit l
   compile (IfE c t e) = do
     c <- compile c
