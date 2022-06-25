8f9f86bc3abcfc663c58f52507791174cf60949c
thomasvergne <thomas.vergne.contact@gmail.com>
2022-06-25 12:33:24 +0200

    ✨ Added main package entry.

---
+++ app/Core/Quark.hs
@@ -0,0 +1,5 @@
+module Core.Quark (module Quark) where
+  import Core.Parser.Parser as Quark
+  import Core.Macro.Compiler as Quark
+  import Core.Macro.Initializing as Quark
+  import Core.Macro.Definition as Quark
