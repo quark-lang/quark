2a135119b487b4abfbddf4f0196e4b39ca032f2a
thomasvergne <thomas.vergne.contact@gmail.com>
2022-06-25 14:38:23 +0200

    ➕ Imported import modules into Quark.

--- app/Core/Quark.hs
+++ app/Core/Quark.hs
@@ -1,5 +1,9 @@
 module Core.Quark (module Quark) where
   import Core.Parser.Parser as Quark
+  
   import Core.Macro.Compiler as Quark
   import Core.Macro.Initializing as Quark
   import Core.Macro.Definition as Quark
+
+  import Core.Import.Duplicates as Quark
+  import Core.Import.Remover as Quark
