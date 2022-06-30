fdbf05ba685a71d0a1c35d958084b6ad6465dcad
thomasvergne <thomas.vergne.contact@gmail.com>
2022-06-30 10:53:12 +0200

    ✨ Implemented STD path support.

--- app/Core/Import/Duplicates.hs
+++ app/Core/Import/Duplicates.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 module Core.Import.Duplicates where
   import Core.Import.Type
   import Core.Parser.AST
@@ -6,18 +7,23 @@ module Core.Import.Duplicates where
   import Text.Megaparsec (ParseErrorBundle)
   import Data.Void (Void)
   import Core.Import.Remover (runImportRemover)
+  import System.Environment (lookupEnv)
   
   type Import = (Either (ParseErrorBundle String Void) ImportMap)
 
   getImportMap :: Path -> Expression -> IO Import
   getImportMap dir (Node (Identifier "import") [Literal (String path)]) = do
-    let src = dir </> path
-    content <- readFile src
+    path' <- case path of
+      's':'t':'d':':':path -> lookupEnv "QUARK" >>= \case
+        Nothing -> error "QUARK environment variable not set"
+        Just quark -> return $ quark </> "library" </> path
+      _ -> return $ dir </> path
+    content <- readFile path'
     case parseLisp content of
       Left err -> return $ Left err
       Right ast -> do 
-        x <- getImports (takeDirectory src) ast
-        return $ appendPath (src, runImportRemover ast) <$> x
+        x <- getImports (takeDirectory path') ast
+        return $ appendPath (path', runImportRemover ast) <$> x
   getImportMap _ _ = return . pure $ []
 
   getImports :: Path -> [Expression] -> IO Import
