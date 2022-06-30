031976978e742d3c7e5bedda7ba3f04b5b45bd35
thomasvergne <thomas.vergne.contact@gmail.com>
2022-06-30 10:49:37 +0200

    ✨ Added Javascript IR generation algorithm.

---
+++ app/Core/Compiler/Definition/Generation.hs
@@ -0,0 +1,41 @@
+module Core.Compiler.Definition.Generation where
+  import Core.Compiler.Definition.IR (Expression(..))
+  import Core.Inference.Type.AST (Literal(..))
+  import Data.List
+  import Text.Printf (printf)
+  import Data.Char (isLetter, ord)
+  
+  varify :: String -> String
+  varify x = "$" ++ concatMap (\x -> if isLetter x then [x] else show (ord x)) x
+
+  encodeUnicode16 :: String -> String
+  encodeUnicode16 = concatMap escapeChar
+    where
+      escapeChar c
+        | ' ' <= c && c <= 'z' = [c]
+        | otherwise = printf "\\u%04x" (fromEnum c)
+        
+  from :: Expression -> String
+  from (Lambda args expr@(Object _)) = "(" ++ intercalate "," args ++ ") => (" ++ from expr ++ ")"
+  from (Lambda args expr) = "(" ++ intercalate "," args ++ ") => " ++ from expr
+  from (Var n) = n
+  from (Object props) = "{" ++ intercalate "," (map (\(p, v) -> p  ++ ": " ++ from v) props) ++ "}"
+  from (Ternary t c e) = from t ++ " ? " ++ from c ++ " : " ++ from e
+  from (Array exprs) = "[" ++ intercalate "," (map from exprs) ++ "]"
+  from (Call (Var n) xs) = n ++ "(" ++ intercalate "," (map from xs) ++ ")"
+  from (Call z@(Property _ _) xs) = from z ++ "(" ++ intercalate "," (map from xs) ++ ")"
+  from (Call n xs) = "(" ++ from n ++ ")(" ++ intercalate "," (map from xs) ++ ")"
+  from (Property n z@(Lit _)) = from n ++ "[" ++ from z ++ "]"
+  from (Property n p) = from n ++ "." ++ from p
+  from (BinaryCall l op r) = from l ++ " " ++ op ++ " " ++ from r
+  from (Let n v) = "var " ++ n ++ " = " ++ from v
+  from (Condition c t) = "if (" ++ from c ++ ") " ++ from t
+  from (Return e) = "return " ++ from e
+  from (Block exprs) = "{" ++ concatMap ((++";") . from) exprs ++ "}"
+  from (Throw e) = "throw " ++ from e
+  from (Lit (S s)) = "\"" ++ encodeUnicode16 s ++ "\""
+  from (Lit (F f)) = show f
+  from (Lit (I i)) = show i
+  from (Require p) = "Object.entries(require(\"" ++ p ++ "\")).map(([name, exported]) => global[name] = exported);"
+  from (Raw c) = c
+  from _ = error "from: not implemented"
---
+++ app/Core/Compiler/Definition/IR.hs
@@ -0,0 +1,31 @@
+module Core.Compiler.Definition.IR where
+  import Core.Inference.Type.AST
+  import Core.Inference.Type.Pretty ()
+  import qualified Data.Map as M
+  import Control.Monad.State
+
+  type Constructors = M.Map String String
+  type MonadCompiler m = (MonadState Constructors m, Monad m, MonadIO m)
+
+  data Expression
+    -- Expressions
+    = Lambda [String] Expression
+    | Var String
+    | Lit Literal
+    | Object [(String, Expression)]
+    | Ternary Expression Expression Expression
+    | Array [Expression]
+    | Call Expression [Expression]
+    | Property Expression Expression
+    | BinaryCall Expression String Expression
+
+    -- Statements
+    | Let String Expression
+    | Function String [String] Expression
+    | Condition Expression Expression
+    | Return Expression
+    | Block [Expression]
+    | Throw Expression
+    | Require String
+    | Raw String
+    deriving (Show, Eq)
