d83467037ea7309efdaa6bcc3f03857cb31e4089
thomasvergne <thomas.vergne.contact@gmail.com>
2022-07-08 13:19:58 +0200

    ✨ Added fully functional pattern matching.

--- app/Core/Compiler/Modules/Pattern.hs
+++ app/Core/Compiler/Modules/Pattern.hs
@@ -7,9 +7,19 @@ module Core.Compiler.Modules.Pattern where
   import Control.Monad.State (get)
   import Data.Maybe (fromJust, isJust)
   import Prelude hiding (and)
+  import Debug.Trace (traceShow)
   
   and :: Expression -> Expression -> Expression
   and l = BinaryCall l "&&"
+
+  -- Returning (Bind, Condition)
+  findPattern :: TypedPattern -> Expression -> [(Maybe Expression, Maybe Expression)]
+  findPattern (LitP l _) e    = [(Nothing, Just $ BinaryCall e "==" (Lit l))]
+  findPattern (WilP _) e      = [(Nothing, Nothing)]
+  findPattern (VarP v _) e    = [(Just $ Let (varify v) e, Nothing)]
+  findPattern (AppP n xs _) e = traceShow (xs, e)
+    concat $ [(Nothing, Just $ BinaryCall (Property e (Var "type")) "==" (Lit (S n)))] : zipWith (\x y -> findPattern y (Property e (Var x)))
+    (["v" ++ show i | i <- [0..]]) xs
 
   compileCase :: MonadCompiler m => TypedPattern -> m (Expression -> Expression -> Expression)
   compileCase (VarP n _) = do
@@ -19,20 +29,15 @@ module Core.Compiler.Modules.Pattern where
           in Condition cond $ Return body
       Nothing -> return $ \x body -> Block [Let n x, Return body]
   compileCase (AppP n args _) = do
-    let args' = zipWith (curry (\case
-                  (VarP n _, i) -> (Just $ \x -> Let (varify n) $ Property x (Var i), Nothing)
-                  (LitP l _, i) -> (Nothing, Just $ \x ->
-                    BinaryCall (Property x (Var i)) "===" (Lit l))
-                  (WilP _, i) -> (Nothing, Nothing)
-                  _ -> error "Pattern must be one level")) args (["v" ++ show i | i <- [0..]])
-    let lets   = map (fromJust . fst) $ filter (isJust . fst) args'
-    let cs = map (fromJust . snd) $ filter (isJust . snd) args'
-    return $ \x b ->
+    return $ \x b -> do
+      let args' = concat $ zipWith (\arg v -> findPattern arg (Property x (Var v))) args (["v" ++ show i | i <- [0..]])
+      let lets   = map (fromJust . fst) $ filter (isJust . fst) args'
+      let cs = map (fromJust . snd) $ filter (isJust . snd) args'
       let cond  = BinaryCall (Property x (Var "type")) "===" (Lit (S n))
           conds = case cs of
-                    (c:cs) -> cond `and` foldl (\acc f -> acc `and` f x) (c x) cs
+                    (c:cs) -> cond `and` foldl and c cs
                     _ -> cond
-        in Condition conds $ Block $ map (\f -> f x) lets ++ [Return b]
+        in Condition conds $ Block $ lets ++ [Return b]
   compileCase (WilP _) = do
     return $ \x b -> Return b
   compileCase (LitP l _) = do
