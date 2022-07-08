64dc7445228c3ce2a97b57ff02ea6ee934eaa0c6
thomasvergne <thomas.vergne.contact@gmail.com>
2022-07-08 18:19:14 +0200

    ✨ Added spreading list operator.

--- app/Core/Utility/Sugar.hs
+++ app/Core/Utility/Sugar.hs
@@ -1,11 +1,12 @@
 module Core.Utility.Sugar where
   import Core.Parser.AST (Expression(..))
-
+  import Data.List (isPrefixOf)
+  
   eliminateSugar :: Expression -> Expression
   eliminateSugar (Node (Identifier "begin") xs) = buildBeginSugar xs
   eliminateSugar (Node (Identifier "match") (pat:cases)) =
     let cases' = map (\(List [pat, expr]) -> List [eliminateSugar pat, eliminateSugar expr]) cases
-      in Node (Identifier "match") (pat:cases')
+      in Node (Identifier "match") (eliminateSugar pat:cases')
   eliminateSugar (Node (Identifier "fn") [args, body]) = Node (Identifier "fn") [args, eliminateSugar body]
   eliminateSugar z@(Node (Identifier "data") _) = z
   eliminateSugar (Node n xs) = Node (eliminateSugar n) (map eliminateSugar xs)
@@ -20,4 +21,5 @@ module Core.Utility.Sugar where
 
   buildList :: [Expression] -> Expression
   buildList [] = Identifier "Nil"
+  buildList [x, Identifier y] = if "*" `isPrefixOf` y then Node (Identifier "Cons") [eliminateSugar x, Identifier $ drop 1 y] else Node (Identifier "Cons") [eliminateSugar x, Node (Identifier "Cons") [Identifier y, Identifier "Nil"]]
   buildList (x:xs) = Node (Identifier "Cons") [eliminateSugar x, buildList xs]
