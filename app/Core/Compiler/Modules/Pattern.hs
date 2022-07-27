d56a71a59a669536ad96540f80cdd0134bb98a68
thomasvergne <thomas.vergne.contact@gmail.com>
2022-07-27 19:31:53 +0200

    🚑 Fixed critical issue in pattern matching.

--- app/Core/Compiler/Modules/Pattern.hs
+++ app/Core/Compiler/Modules/Pattern.hs
@@ -3,7 +3,7 @@ module Core.Compiler.Modules.Pattern where
   import Core.Inference.Type.AST (Literal(S), TypedPattern(..))
   import Core.Compiler.Definition.Generation
   import qualified Data.Map as M
-  import Control.Monad.State (get)
+  import Control.Monad.State (get, zipWithM, MonadIO (liftIO))
   import Data.Maybe (fromJust, isJust)
   import Prelude hiding (and)
   import Debug.Trace (traceShow)
@@ -12,14 +12,16 @@ module Core.Compiler.Modules.Pattern where
   and l = BinaryCall l "&&"
 
   -- Returning (Bind, Condition)
-  findPattern :: TypedPattern -> Expression -> [(Maybe Expression, Maybe Expression)]
-  findPattern (VarP "true" _) e  = [(Nothing, Just e)]
-  findPattern (VarP "false" _) e = [(Nothing, Just $ Call (Var "!") [e])]
-  findPattern (LitP l _) e       = [(Nothing, Just $ BinaryCall e "==" (Lit l))]
-  findPattern (WilP _) e         = [(Nothing, Nothing)]
-  findPattern (VarP v _) e       = [(Just $ Let (varify v) e, Nothing)]
-  findPattern (AppP n xs _) e    = 
-    concat $ [(Nothing, Just $ BinaryCall (Property e (Var "type")) "==" (Lit (S n)))] : zipWith (\x y -> findPattern y (Property e (Var x)))
+  findPattern :: Constructors -> TypedPattern -> Expression -> [(Maybe Expression, Maybe Expression)]
+  findPattern env (VarP "true" _) e  = [(Nothing, Just e)]
+  findPattern env (VarP "false" _) e = [(Nothing, Just $ Call (Var "!") [e])]
+  findPattern env (LitP l _) e       = [(Nothing, Just $ BinaryCall e "==" (Lit l))]
+  findPattern env (WilP _) e         = [(Nothing, Nothing)]
+  findPattern env (VarP v _) e       = case M.lookup (varify v) env of
+    Just _ -> [(Nothing, Just $ BinaryCall (Property e (Var "type")) "==" (Lit $ S v))]
+    Nothing -> [(Just $ Let (varify v) e, Nothing)]
+  findPattern env (AppP n xs _) e    = 
+    concat $ [(Nothing, Just $ BinaryCall (Property e (Var "type")) "==" (Lit (S n)))] : zipWith (\x y -> findPattern env y (Property e (Var x)))
     (["v" ++ show i | i <- [0..]]) xs
 
   compileCase :: MonadCompiler m => TypedPattern -> m (Expression -> Expression -> Expression)
@@ -32,8 +34,9 @@ module Core.Compiler.Modules.Pattern where
           in Condition cond $ Return body
       Nothing -> return $ \x body -> Block [Let n x, Return body]
   compileCase (AppP n args _) = do
+    e <- get
     return $ \x b -> do
-      let args' = concat $ zipWith (\arg v -> findPattern arg (Property x (Var v))) args (["v" ++ show i | i <- [0..]])
+      let args' = concat $ zipWith (\arg v -> findPattern e arg (Property x (Var v))) args (["v" ++ show i | i <- [0..]])
       let lets   = map (fromJust . fst) $ filter (isJust . fst) args'
       let cs = map (fromJust . snd) $ filter (isJust . snd) args'
       let cond  = BinaryCall (Property x (Var "type")) "===" (Lit (S n))
