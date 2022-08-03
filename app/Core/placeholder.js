38093a418ad55256856dbf1bd884612467781e79
thomasvergne <thomas.vergne.contact@gmail.com>
2022-08-03 13:09:57 +0200

    ✨ Added some show string functions.

--- app/Core/placeholder.js
+++ app/Core/placeholder.js
@@ -1,1 +1,10 @@
-$main();
+const toString = str => {
+  if (str.type === 'Cons') {
+    const x = str.v0;
+    const xs = str.v1;
+    return `${x}${toString(xs)}`;
+  }
+  if (str.type === 'Nil') return '';
+}
+
+const read = str => Number(toString(str));
