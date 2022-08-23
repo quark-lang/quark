9ad0ddbe77089e7b8ffc94d61c51bd6de5a1b3bf
thomasvergne <thomas.vergne.contact@gmail.com>
2022-08-23 18:31:12 +0200

    🚑 Fixed parser lexing issue.

--- app/Core/Parser/Parser.hs
+++ app/Core/Parser/Parser.hs
@@ -2,7 +2,7 @@
 module Core.Parser.Parser where
   import Core.Parser.AST
   import Text.Megaparsec
-  import Text.Megaparsec.Char (char, digitChar, space, space1, alphaNumChar)
+  import Text.Megaparsec.Char (char, digitChar, space, space1, alphaNumChar, eol)
   import Data.Maybe           (isJust, fromMaybe, fromJust, catMaybes)
   import Data.Void
   import Control.Monad        (void)
@@ -10,7 +10,7 @@ module Core.Parser.Parser where
   import Debug.Trace          (traceShow)
   import Data.Functor
   import qualified Text.Megaparsec.Char.Lexer as L
-  
+
   {-
     Module: Quark parser
     Description: Lisp like parser using custom combinator library
@@ -27,86 +27,76 @@ module Core.Parser.Parser where
   lexeme :: MonadParsec Void String m => m a -> m a
   lexeme = L.lexeme sc
 
-  stringLit :: MonadParsec Void String m => m (Maybe Literal)
+  stringLit :: MonadParsec Void String m => m Literal
   stringLit = do
     char '"'
     s <- manyTill L.charLiteral (char '"')
-    return . Just $ String s
+    return $ String s
 
-  integerLit :: MonadParsec Void String m => m (Maybe Literal)
+  integerLit :: MonadParsec Void String m => m Literal
   integerLit = do
     n <- some digitChar
-    return . Just $ Integer (read n)
-  
-  charLit :: MonadParsec Void String m => m (Maybe Literal)
+    return $ Integer (read n)
+
+  charLit :: MonadParsec Void String m => m Literal
   charLit = do
     char '\''
     c <- L.charLiteral
     char '\''
-    return . Just $ Char c
+    return $ Char c
 
-  floatLit :: MonadParsec Void String m => m (Maybe Literal)
+  floatLit :: MonadParsec Void String m => m Literal
   floatLit = do
     n <- some digitChar
     char '.'
     d <- some digitChar
-    return . Just $ Float (read (n ++ "." ++ d))
+    return $ Float (read (n ++ "." ++ d))
 
   blacklist :: String
   blacklist = "() {}[];\r\n\t\"@"
 
-  literal :: MonadParsec Void String m => m (Maybe Literal)
+  literal :: MonadParsec Void String m => m Literal
   literal = charLit <|> stringLit <|> try floatLit <|> integerLit
 
-  identifier :: MonadParsec Void String m => m (Maybe Expression)
-  identifier = Just . Identifier <$> some (noneOf blacklist)
+  identifier :: MonadParsec Void String m => m Expression
+  identifier = Identifier <$> some (noneOf blacklist)
 
-  atom :: MonadParsec Void String m => m (Maybe Expression)
-  atom = literal >>= \case
-    Just x -> return . Just . Literal $ x
-    Nothing -> return Nothing
+  atom :: MonadParsec Void String m => m Expression
+  atom = literal <&> Literal
 
-  expr :: MonadParsec Void String m => m (Maybe Expression)
+  expr :: MonadParsec Void String m => m Expression
   expr = do
     char '('
     x <- many parse'
     char ')'
-    Just <$> let xs = catMaybes x
-      in case xs of
-        (x:xs) -> return $ Node x xs
-        [] -> return (Identifier "nil")
+    case x of
+      x:xs -> return $ Node x xs
+      [] -> return $ Identifier "Void"
 
-  begin :: MonadParsec Void String m => m (Maybe Expression)
+  begin :: MonadParsec Void String m => m Expression
   begin = do
     char '{' >> space
     x <- many parse'
     space >> char '}'
-    Just <$> let xs = catMaybes x
-      in return (Node (Identifier "begin") xs)
+    return (Node (Identifier "begin") x)
 
-  list :: MonadParsec Void String m => m (Maybe Expression)
+  list :: MonadParsec Void String m => m Expression
   list = do
     char '[' >> space
     x <- many parse' <* space
     space >> char ']'
-    let xs = catMaybes x
-      in return . Just $ List xs
+    return $ List x
 
-  quoted :: MonadParsec Void String m => m (Maybe Expression)
+  quoted :: MonadParsec Void String m => m Expression
   quoted = do
     char '@'
-    x <- parse'
-    return $ Quoted <$> x
+    Quoted <$> parse'
 
-  comment :: MonadParsec Void String m => m (Maybe Expression)
-  comment = do
-    char ';' >> manyTill anySingle (void (char '\n') <|> eof)
-    return Nothing
 
-  parse' :: MonadParsec Void String m => m (Maybe Expression)
+  parse' :: MonadParsec Void String m => m Expression
   parse' = lexeme (list <|> begin <|> expr <|> atom <|> quoted <|> identifier) <?> ""
 
   parseLisp :: String -> Either (ParseErrorBundle String Void) [Expression]
-  parseLisp = parse (catMaybes <$> many (space *> parse' <* space)) ""
+  parseLisp = parse (many (sc *> parse' <* sc)) ""
 
   
